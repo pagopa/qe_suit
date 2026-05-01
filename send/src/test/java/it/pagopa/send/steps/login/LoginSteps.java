1e3ab3968001e997bfee5c26c5474427a7dcf62f
Francesco Sinopoli <francesco.sinopoli@pagopa.it>
2026-05-01 15:32:51 +0200

    feat(send): add ReserverdAreaPage and selectSEND login step

--- send/src/test/java/it/pagopa/send/steps/login/LoginSteps.java
+++ send/src/test/java/it/pagopa/send/steps/login/LoginSteps.java
@@ -6,11 +6,14 @@ import it.frontend.e2e.framework.web.WebPresentationGateway;
 import it.pagopa.send.enums.User;
 import it.pagopa.send.steps.login.page.DashboardPartySelectionPage;
 import it.pagopa.send.steps.login.page.OneIdPage;
+import it.pagopa.send.steps.login.page.ReserverdAreaPage;
+import it.pagopa.send.steps.mittenti.creazione_notifica.NotificationContext;
+import it.pagopa.send.steps.mittenti.pages.DashboardPage;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class LoginSteps {
-
+    private final NotificationContext context;
     private final WebPresentationGateway uiGateway;
 
     @Given("l'utente {user} effettua l'accesso a SelfCare con autenticazione SPID")
@@ -24,5 +27,14 @@ public class LoginSteps {
     public void selectPa(String comune) {
         DashboardPartySelectionPage partyPage = uiGateway.bind(DashboardPartySelectionPage.class);
         partyPage.selectComune(comune);
+    }
+
+    @When("l'utente accede alla area riservata e seleziona il prodotto SEND")
+    public void selectSEND() {
+        ReserverdAreaPage nextPage = uiGateway.bind(ReserverdAreaPage.class);
+        nextPage.assertLoaded();
+        nextPage.accediToSend();
+        DashboardPage page = uiGateway.bind(DashboardPage.class);
+        page.assertLoadedWithBannerCheck();
     }
 }
---
+++ send/src/test/java/it/pagopa/send/steps/login/page/ReserverdAreaPage.java
@@ -0,0 +1,31 @@
+package it.pagopa.send.steps.login.page;
+
+import it.frontend.e2e.framework.annotation.location.web.Url;
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.core.capability.core.Clickable;
+import it.frontend.e2e.framework.web.capability.core.Readable;
+import it.frontend.e2e.framework.web.domain.Page;
+import org.assertj.core.api.Assertions;
+
+@Url("sottoinsieme di ${url.selfcare.notifiche.base}")
+public interface ReserverdAreaPage extends Page {
+
+    @XPath("//*[@id=\"root\"]/div/div[1]/nav/div/div/div/div[1]/div/div/p")
+    Readable<String> header();
+
+    @XPath("//*[@id=\"forward_prod-pn\"]")
+    Clickable accediSendButton();
+
+    default void accediToSend() {
+        accediSendButton().click();
+    }
+    
+    @Override
+    default void assertLoaded() {
+        header().readAndAssert(h -> {
+            Assertions.assertThat(h).isNotNull();
+            Assertions.assertThat(h.getText())
+                      .isIn("Area Riservata", "Area Riservata");
+        });
+    }
+}
