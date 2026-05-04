4589440ad0806a95a2b2b0ac50cb97fdeaa43ede
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-04 14:50:43 +0200

    feat(/interop): add initial configuration files for QA and DEV profiles, including URL settings and logging configuration

---
+++ interop/src/test/java/it/pagopa/send/TestBootApp.java
@@ -0,0 +1,11 @@
+package it.pagopa.send;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"it.pagopa.send"})
+public class TestBootApp {
+}
+
+
