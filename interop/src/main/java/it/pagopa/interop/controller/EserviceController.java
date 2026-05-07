1adb9dce079f2caccb20e239231948ed99c0d052
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 22:17:26 +0200

    feat(/interop): enhance EserviceController to utilize User enum for tenant admin context

--- interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
+++ interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
@@ -2,9 +2,26 @@ package it.pagopa.interop.controller;
 
 import io.cucumber.java.en.Given;
 import it.pagopa.interop.domain.enums.Tenant;
+import it.pagopa.interop.domain.enums.User;
+import it.pagopa.interop.domain.services.eservice.EserviceService;
+import it.pagopa.interop.infrastructure.client.auth.context.user.CurrentUserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class EserviceController {
+
+    private final EserviceService service;
+    private final CurrentUserContext currentUserContext;
+
     @Given("un eservice creato da {tenant} con una richiesta di fruizione e una finalità associate da {tenant}")
     public void setupEservice(Tenant producer, Tenant consumer) {
+        User producerAdmin = User.getTenantAdmin(producer);
+        currentUserContext.set(producerAdmin, producer);
+        service.createEservice();
+
+        User consumerAdmin = User.getTenantAdmin(consumer);
+        currentUserContext.set(consumerAdmin, consumer);
+       //TODO: richiesta di fruizione e finalità
     }
 }
