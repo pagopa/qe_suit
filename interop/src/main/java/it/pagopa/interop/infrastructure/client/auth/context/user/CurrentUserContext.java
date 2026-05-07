3d7a2aad6cc86b11555a6f442c5bc563840c0cc5
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 22:17:21 +0200

    feat(/interop): refactor BearerAuthProvider and CurrentUserContext to use User and Tenant enums

--- interop/src/main/java/it/pagopa/interop/infrastructure/client/auth/context/user/CurrentUserContext.java
+++ interop/src/main/java/it/pagopa/interop/infrastructure/client/auth/context/user/CurrentUserContext.java
@@ -1,17 +1,28 @@
 package it.pagopa.interop.infrastructure.client.auth.context.user;
 
 import io.cucumber.spring.ScenarioScope;
+import it.pagopa.interop.domain.enums.Tenant;
+import it.pagopa.interop.domain.enums.User;
 import org.springframework.stereotype.Component;
 
 @Component
 @ScenarioScope
 public class CurrentUserContext {
-    private CurrentUser currentUser;
+    private User currentUser;
+    private Tenant currentTenant;
 
-    public void set(CurrentUser user) { this.currentUser = user; }
+    public void set(User user, Tenant tenant) {
+        this.currentUser = user;
+        this.currentTenant = tenant;
+    }
 
-    public CurrentUser getUser() {
+    public User getUser() {
         if (currentUser == null) throw new IllegalStateException("Current user is not set");
         return currentUser;
+    }
+
+    public Tenant getTenant() {
+        if (currentTenant == null) throw new IllegalStateException("Current tenant is not set");
+        return currentTenant;
     }
 }
