a26960b9de46b991b8e970c1d81503f33b0a25ad
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 22:17:16 +0200

    feat(/interop): update service interfaces to accept generic request objects

--- interop/src/main/java/it/pagopa/interop/domain/services/client/ClientService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/client/ClientService.java
@@ -1,8 +1,7 @@
 package it.pagopa.interop.domain.services.client;
 
 import it.pagopa.interop.domain.model.Client;
-import it.pagopa.interop.domain.model.requests.ClientCreationRequest;
 
 public interface ClientService {
-    Client createClient(ClientCreationRequest request);
+    Client createClient(Object request);
 }
