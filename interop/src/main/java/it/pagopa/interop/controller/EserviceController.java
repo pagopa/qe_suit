5a663391b8f055ffe029d9065a802f913476c21b
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 13:20:44 +0200

    feat(/interop): update setupEservice method to use Tenant enum for tenant handling

--- interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
+++ interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
@@ -1,9 +1,10 @@
 package it.pagopa.interop.controller;
 
 import io.cucumber.java.en.Given;
+import it.pagopa.interop.domain.enums.Tenant;
 
 public class EserviceController {
-    @Given("un eservice creato da {string} con una richiesta di fruizione e una finalità associate da {string}")
-    public void setupEservice(User produerUser, User consumerUser) {
+    @Given("un eservice creato da {tenant} con una richiesta di fruizione e una finalità associate da {tenant}")
+    public void setupEservice(Tenant produer, Tenant consumer) {
     }
 }
