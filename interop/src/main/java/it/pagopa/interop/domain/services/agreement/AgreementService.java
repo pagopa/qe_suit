6a21bedeaa505f994e0689ece93dae9154cfb963
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 22:17:16 +0200

    feat(/interop): update service interfaces to accept generic request objects

--- interop/src/main/java/it/pagopa/interop/domain/services/agreement/AgreementService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/agreement/AgreementService.java
@@ -2,8 +2,7 @@ package it.pagopa.interop.domain.services.agreement;
 
 import it.pagopa.interop.domain.model.Agreement;
 import it.pagopa.interop.domain.model.Eservice;
-import it.pagopa.interop.domain.model.requests.AgreementCreationRequest;
 
 public interface AgreementService {
-    Agreement createEserviceAgreement(Eservice eservice, AgreementCreationRequest request);
+    Agreement createEserviceAgreement(Eservice eservice, Object request);
 }
--- interop/src/main/java/it/pagopa/interop/domain/services/purpose/PurposeService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/purpose/PurposeService.java
@@ -3,10 +3,8 @@ package it.pagopa.interop.domain.services.purpose;
 import it.pagopa.interop.domain.model.Client;
 import it.pagopa.interop.domain.model.Eservice;
 import it.pagopa.interop.domain.model.Purpose;
-import it.pagopa.interop.domain.model.requests.PurposeCreationRequest;
-
 
 public interface PurposeService {
-    Purpose createEservicePurpose(Eservice eservice, PurposeCreationRequest request);
+    Purpose createEservicePurpose(Eservice eservice, Object request);
     Purpose associatePurposeToClient(Purpose purpose, Client client);
 }
