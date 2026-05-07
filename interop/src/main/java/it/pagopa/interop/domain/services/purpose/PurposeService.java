8c7ff5d8dd1b724d1ee79c2ecdf397a9a17d3db7
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 14:47:00 +0200

    feat(/interop): add DTO imports for PurposeService interface

--- interop/src/main/java/it/pagopa/interop/domain/services/purpose/PurposeService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/purpose/PurposeService.java
@@ -1,5 +1,11 @@
 package it.pagopa.interop.domain.services.purpose;
 
+import it.pagopa.interop.domain.dto.Client;
+import it.pagopa.interop.domain.dto.Eservice;
+import it.pagopa.interop.domain.dto.Purpose;
+import it.pagopa.interop.domain.dto.requests.PurposeCreationRequest;
+
+
 public interface PurposeService {
     Purpose createEservicePurpose(Eservice eservice, PurposeCreationRequest request);
     Purpose associatePurposeToClient(Purpose purpose, Client client);
