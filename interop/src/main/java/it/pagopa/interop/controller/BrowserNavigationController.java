afcc52b39028956de74889ea64194cba6673b3cf
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 14:17:00 +0200

    feat(/interop): enhance navigation and client assertion handling with user roles and tenant parameters

--- interop/src/main/java/it/pagopa/interop/controller/BrowserNavigationController.java
+++ interop/src/main/java/it/pagopa/interop/controller/BrowserNavigationController.java
@@ -1,11 +1,14 @@
 package it.pagopa.interop.controller;
 
 import io.cucumber.java.en.When;
+import it.frontend.e2e.framework.web.domain.Page;
+import it.pagopa.interop.domain.enums.Tenant;
+import it.pagopa.interop.domain.enums.UserRole;
 
 public class BrowserNavigationController {
 
-    @When("{string} si trova alla pagina {string} del portale Interop")
-    public void navigateToPage(Page page){
+    @When("l'utente {userRole} di {tenant} si trova alla pagina {page} del portale Interop")
+    public void navigateToPage(UserRole user, Tenant tenant, Page page){
 
     }
 }
