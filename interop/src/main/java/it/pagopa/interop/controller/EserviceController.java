c7c5b6aa2c5995b400eb04df38933ba8f3b1ab21
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 14:47:04 +0200

    fix(/interop): correct typo in setupEservice method parameter name

--- interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
+++ interop/src/main/java/it/pagopa/interop/controller/EserviceController.java
@@ -5,6 +5,6 @@ import it.pagopa.interop.domain.enums.Tenant;
 
 public class EserviceController {
     @Given("un eservice creato da {tenant} con una richiesta di fruizione e una finalità associate da {tenant}")
-    public void setupEservice(Tenant produer, Tenant consumer) {
+    public void setupEservice(Tenant producer, Tenant consumer) {
     }
 }
