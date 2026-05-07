48072d92a9ee7fccd4bc4dc5dd147f3bd597eda9
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-07 12:33:09 +0200

    feat(/interop): add BrowserNavigationController, ClientAssertionController, ClientController, and EserviceController with Cucumber step definitions

---
+++ interop/src/main/java/it/pagopa/interop/controller/BrowserNavigationController.java
@@ -0,0 +1,11 @@
+package it.pagopa.interop.controller;
+
+import io.cucumber.java.en.When;
+
+public class BrowserNavigationController {
+
+    @When("{string} si trova alla pagina {string} del portale Interop")
+    public void navigateToPage(Page page){
+
+    }
+}
