ad0015974757d72f01ec3032f7591043d8d4bc69
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 21:45:35 +0200

    feat(/interop): refactor client assertion validation and introduce DevTools service for enhanced functionality

--- interop/src/main/java/it/pagopa/interop/controller/ClientAssertionController.java
+++ interop/src/main/java/it/pagopa/interop/controller/ClientAssertionController.java
@@ -1,18 +1,13 @@
 package it.pagopa.interop.controller;
 
 import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
-import io.cucumber.java.PendingException;
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 import it.pagopa.interop.domain.context.ClientAssertionContext;
 import it.pagopa.interop.domain.model.Client;
 import it.pagopa.interop.domain.model.ClientAssertion;
-import it.pagopa.interop.domain.model.ClientAssertionValidationResult;
 import it.pagopa.interop.domain.model.Purpose;
 import it.pagopa.interop.domain.services.client_assertion.CreateClientAssertionService;
-import it.pagopa.interop.domain.services.client_assertion.impl.WebClientAssertionService;
+import it.pagopa.interop.domain.services.client_assertion.impl.WebDevToolsService;
 import it.pagopa.interop.utils.JwtBuilderUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class ClientAssertionController {
     private final CreateClientAssertionService clientAssertionService;
-    private final WebClientAssertionService webClientAssertionService;
+    private final WebDevToolsService webClientAssertionService;
     private final ClientAssertionContext clientAssertionContext;
 
     @Given("una client assertion valida generata usando il {currentClient} e la {currentPurpose}")
@@ -39,23 +32,4 @@ public class ClientAssertionController {
         String clientAssertion = clientAssertionService.createClientAssertion(client, purpose, overrides);
         clientAssertionContext.upsert(new ClientAssertion(clientAssertion));
     }
-
-    @When("l'utente richiede la validazione della {currentClientAssertion} associata al {currentClient}")
-    public void validateClientAssertion(ClientAssertion clientAssertion, Client client) {
-        ClientAssertionValidationResult result =
-                webClientAssertionService.validateClientAssertion(clientAssertion, client);
-
-        clientAssertionContext.addValidation(clientAssertion, result);
-    }
-
-
-    @Then("i risultati della validazione della {currentClientAssertion} sono:")
-    public void checkValidationResult(ClientAssertion clientAssertion, ClientAssertionValidationResult expected) {
-        ClientAssertionValidationResult actual = clientAssertionContext.getValidation(clientAssertion);
-
-        assertThat(actual)
-                .as("Validation result for clientAssertion: %s", clientAssertion)
-                .isEqualTo(expected);
-    }
-
 }
---
+++ interop/src/main/java/it/pagopa/interop/controller/DevToolsController.java
@@ -0,0 +1,38 @@
+package it.pagopa.interop.controller;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import it.pagopa.interop.domain.context.ClientAssertionContext;
+import it.pagopa.interop.domain.model.Client;
+import it.pagopa.interop.domain.model.ClientAssertion;
+import it.pagopa.interop.domain.model.ClientAssertionValidationResult;
+import it.pagopa.interop.domain.services.client_assertion.impl.WebDevToolsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class DevToolsController {
+    private final WebDevToolsService webClientAssertionService;
+    private final ClientAssertionContext clientAssertionContext;
+
+    @When("l'utente richiede la validazione della {currentClientAssertion} associata al {currentClient}")
+    public void validateClientAssertion(ClientAssertion clientAssertion, Client client) {
+        ClientAssertionValidationResult result =
+                webClientAssertionService.validate(clientAssertion, client);
+
+        clientAssertionContext.addValidation(clientAssertion, result);
+    }
+
+
+    @Then("i risultati della validazione della {currentClientAssertion} sono:")
+    public void checkValidationResult(ClientAssertion clientAssertion, ClientAssertionValidationResult expected) {
+        ClientAssertionValidationResult actual = clientAssertionContext.getValidation(clientAssertion);
+
+        assertThat(actual)
+                .as("Validation result for clientAssertion: %s", clientAssertion)
+                .isEqualTo(expected);
+    }
+
+}
