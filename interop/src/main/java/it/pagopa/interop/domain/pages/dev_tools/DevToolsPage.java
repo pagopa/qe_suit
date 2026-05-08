e7fa031c4753c063b9ef0e31b8d2443b63b18f20
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 16:09:08 +0200

    feat(/interop): add DebugClientAssertionPage and DevToolsPage interfaces for development tools

---
+++ interop/src/main/java/it/pagopa/interop/domain/pages/dev_tools/DevToolsPage.java
@@ -0,0 +1,26 @@
+package it.pagopa.interop.domain.pages.dev_tools;
+
+import it.frontend.e2e.framework.annotation.location.web.Url;
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.core.capability.core.Clickable;
+import it.frontend.e2e.framework.web.capability.core.Readable;
+import it.frontend.e2e.framework.web.domain.Page;
+import org.assertj.core.api.Assertions;
+
+@Url("${interop.web.base-url}/tool-sviluppo")
+public interface DevToolsPage extends Page {
+
+    @XPath("//*[@id=\"interop-sidenav-main\"]/div/main/div/div[2]/div/div/h1")
+    Readable<String> pageTitle();
+
+    @XPath("//a[contains(@class,'MuiButton-root') and contains(normalize-space(.), 'Effettua il debug')]")
+    Clickable debugClientAssertionButton();
+
+    @Override
+    default void assertLoaded() {
+        pageTitle().readAndAssert(webElm ->
+                Assertions.assertThat(webElm.getText()).isNotBlank()
+                        .containsIgnoringCase("Tool per lo sviluppo")
+        );
+    }
+}
