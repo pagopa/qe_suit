b9465634b6bb07310d20e1bdfa5d0129a291eef8
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 17:01:54 +0200

    feat(/interop): enhance ClientAssertionContext with validation mapping and update validation method in DebugClientAssertionPage

--- interop/src/main/java/it/pagopa/interop/domain/context/ClientAssertionContext.java
+++ interop/src/main/java/it/pagopa/interop/domain/context/ClientAssertionContext.java
@@ -2,9 +2,18 @@ package it.pagopa.interop.domain.context;
 
 import io.cucumber.spring.ScenarioScope;
 import it.pagopa.interop.domain.model.ClientAssertion;
+import it.pagopa.interop.domain.model.ClientAssertionValidation;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @ScenarioScope
 public class ClientAssertionContext extends AbstractContext<ClientAssertion> {
+    private Map<ClientAssertion, ClientAssertionValidation> validationMap = new HashMap<>();
+
+    public void addValidation(ClientAssertion clientAssertion, ClientAssertionValidation validation) {
+        validationMap.put(clientAssertion, validation);
+    }
 }
