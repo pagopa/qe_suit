da0b5cbd2b1a4b3ea514caa21b025af685b960fc
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 20:07:55 +0200

    feat(/interop): add Chip interface for handling MuiChip components with success and error states

---
+++ interop/src/main/java/it/pagopa/interop/domain/web/commons/component/Chip.java
@@ -0,0 +1,30 @@
+package it.pagopa.interop.domain.web.commons.component;
+
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.web.capability.core.Readable;
+import it.frontend.e2e.framework.web.domain.Component;
+
+@XPath(".//*[contains(@class, 'MuiChip-root')]")
+public interface Chip extends Component {
+    String SUCCESS_CLASS = "MuiChip-colorSuccess";
+    String ERROR_CLASS = "MuiChip-error";
+
+    @XPath(".//span")
+    Readable<String> text();
+
+    default boolean isSuccess() {
+        var inta = 1;
+        return this.get()
+                .stream()
+                .flatMap(el -> el.getClasses().stream())
+                .anyMatch(c -> c.contains(SUCCESS_CLASS));
+    }
+
+    default boolean isError() {
+        return this.get()
+                .stream()
+                .flatMap(el -> el.getClasses().stream())
+                .anyMatch(c -> c.contains(ERROR_CLASS));
+    }
+
+}
