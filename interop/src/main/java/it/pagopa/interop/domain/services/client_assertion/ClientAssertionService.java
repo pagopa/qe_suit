521ede237b6c94e9b3c5cd6495de4c1640e9ab1c
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 16:59:00 +0200

    feat(/interop): update client assertion validation to use ClientAssertion object

--- interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/ClientAssertionService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/ClientAssertionService.java
@@ -1,8 +1,9 @@
 package it.pagopa.interop.domain.services.client_assertion;
 
 import it.pagopa.interop.domain.model.Client;
+import it.pagopa.interop.domain.model.ClientAssertion;
 import it.pagopa.interop.domain.model.ClientAssertionValidation;
 
 public interface ClientAssertionService {
-    ClientAssertionValidation validateClientAssertion(String clientAssertion, Client client);
+    ClientAssertionValidation validateClientAssertion(ClientAssertion clientAssertion, Client client);
 }
--- interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/impl/WebClientAssertionService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/impl/WebClientAssertionService.java
@@ -2,6 +2,7 @@ package it.pagopa.interop.domain.services.client_assertion.impl;
 
 import it.frontend.e2e.framework.web.WebPresentationGateway;
 import it.pagopa.interop.domain.model.Client;
+import it.pagopa.interop.domain.model.ClientAssertion;
 import it.pagopa.interop.domain.model.ClientAssertionValidation;
 import it.pagopa.interop.domain.pages.dev_tools.DebugClientAssertionPage;
 import it.pagopa.interop.domain.services.client_assertion.ClientAssertionService;
@@ -16,9 +17,9 @@ public class WebClientAssertionService implements ClientAssertionService {
     private final WebPresentationGateway webPresentationGateway;
 
     @Override
-    public ClientAssertionValidation validateClientAssertion(String clientAssertion, Client client) {
+    public ClientAssertionValidation validateClientAssertion(ClientAssertion clientAssertion, Client client) {
         DebugClientAssertionPage debugClientAssertionPage = webPresentationGateway.bind(DebugClientAssertionPage.class);
-        debugClientAssertionPage.setClientAssertion(clientAssertion);
+        debugClientAssertionPage.setClientAssertion(clientAssertion.getClientAssertion());
         debugClientAssertionPage.setClientId(client.getId().toString());
         debugClientAssertionPage.validate();
 
