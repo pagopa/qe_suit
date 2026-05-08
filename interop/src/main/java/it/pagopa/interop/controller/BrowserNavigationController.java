f39d18061b13846253c6b46edcf8f55661347856
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-08 15:49:48 +0200

    feat(/interop): implement WebBrowserService for session token management and enhance BrowserNavigationController for user navigation

--- interop/src/main/java/it/pagopa/interop/controller/BrowserNavigationController.java
+++ interop/src/main/java/it/pagopa/interop/controller/BrowserNavigationController.java
@@ -3,12 +3,31 @@ package it.pagopa.interop.controller;
 import io.cucumber.java.en.When;
 import it.frontend.e2e.framework.web.domain.Page;
 import it.pagopa.interop.domain.enums.Tenant;
+import it.pagopa.interop.domain.enums.User;
 import it.pagopa.interop.domain.enums.UserRole;
+import it.pagopa.interop.domain.services.browser.WebBrowserService;
+import it.pagopa.interop.infrastructure.client.auth.bearer.BearerAuthProvider;
+import it.pagopa.interop.infrastructure.client.auth.context.user.CurrentUserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class BrowserNavigationController {
 
-    @When("l'utente {userRole} di {tenant} si trova alla pagina {page} del portale Interop")
-    public void navigateToPage(UserRole user, Tenant tenant, Page page){
+    private final WebBrowserService webBrowserService;
+    private final CurrentUserContext currentUserContext;
+    private final BearerAuthProvider bearerAuthProvider;
 
+    @When("l'utente {userRole} di {tenant} si trova alla pagina {page} del portale Interop")
+    public void navigateToPage(UserRole userRole, Tenant tenant, Page page){
+        User user = User.getTenantUser(tenant, userRole);
+
+        if (!currentUserContext.isLoggedIn(user, tenant) || !webBrowserService.hasSessionToken()) {
+            currentUserContext.set(user, tenant);
+            String sessionToken = bearerAuthProvider.getToken();
+            webBrowserService.setSessionToken(sessionToken);
+        }
+
+        page.navigateTo();
     }
 }
