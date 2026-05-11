351ea26d6310094f09bd6184ec990efcd3d57d35
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-11 19:36:53 +0200

    refactor(/interop): update client assertion validation to support InteropClientType parameter

--- interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/impl/WebDevToolsService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/client_assertion/impl/WebDevToolsService.java
@@ -1,6 +1,7 @@
 package it.pagopa.interop.domain.services.client_assertion.impl;
 
 import it.frontend.e2e.framework.web.WebPresentationGateway;
+import it.pagopa.interop.domain.enums.InteropClientType;
 import it.pagopa.interop.domain.model.Client;
 import it.pagopa.interop.domain.model.ClientAssertion;
 import it.pagopa.interop.domain.model.ClientAssertionValidationResult;
@@ -17,23 +18,24 @@ public class WebDevToolsService implements DevToolsService {
 
     private final WebPresentationGateway webPresentationGateway;
 
+
     @Override
     public ClientAssertionValidationResult validate(ClientAssertion clientAssertion, Client client) {
-        return internalValidate(clientAssertion.getClientAssertion(), client.getId().toString(), null);
+        return internalValidate(clientAssertion.getClientAssertion(), InteropClientType.valueOf(client.getKind().name()), client.getId().toString(), null);
     }
 
     @Override
     public ClientAssertionValidationResult validate(ClientAssertion clientAssertion, Client client, DPoPProof proof) {
-        return internalValidate(clientAssertion.getClientAssertion(), client.getId().toString(), proof.getJwt());
+        return internalValidate(clientAssertion.getClientAssertion(), InteropClientType.valueOf(client.getKind().name()), client.getId().toString(), proof.getJwt());
     }
 
     @Override
-    public ClientAssertionValidationResult validate(String clientAssertion, String client, String proof) {
-        return internalValidate(clientAssertion, client, proof);
+    public ClientAssertionValidationResult validate(String clientAssertion, InteropClientType clientType, String client, String proof) {
+        return internalValidate(clientAssertion,clientType,  client, proof);
     }
 
 
-    private ClientAssertionValidationResult internalValidate(String clientAssertion, String clientId, String dPoPProof) {
+    private ClientAssertionValidationResult internalValidate(String clientAssertion, InteropClientType clientType, String clientId, String dPoPProof) {
         DebugClientAssertionPage debugClientAssertionPage = webPresentationGateway.bind(DebugClientAssertionPage.class);
 
         if(clientAssertion != null)
@@ -44,6 +46,6 @@ public class WebDevToolsService implements DevToolsService {
 
         //TODO: dpop proof
 
-        return debugClientAssertionPage.validate();
+        return debugClientAssertionPage.validate(clientType);
     }
 }
