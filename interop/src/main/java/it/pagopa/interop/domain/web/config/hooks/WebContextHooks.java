7e1be837d13dfa695fbb68bae0eefe19e9e6cb13
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-12 22:26:30 +0200

    chore(/interop): add scenario name to MDC before each scenario and clean up after

--- interop/src/main/java/it/pagopa/interop/domain/web/config/hooks/WebContextHooks.java
+++ interop/src/main/java/it/pagopa/interop/domain/web/config/hooks/WebContextHooks.java
@@ -1,9 +1,11 @@
 package it.pagopa.interop.domain.web.config.hooks;
 
 import io.cucumber.java.After;
-import it.frontend.e2e.framework.core.config.SuiteContext;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import it.frontend.e2e.framework.web.WebPresentationGateway;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -11,8 +13,17 @@ public class WebContextHooks {
 
     private final WebPresentationGateway webPresentationGateway;
 
+    @Before
+    public void beforeScenario(Scenario scenario) {
+        MDC.put("scenario", scenario.getName());
+    }
+
     @After
     public void afterScenario() {
-       webPresentationGateway.close();
+        try {
+            webPresentationGateway.close();
+        } finally {
+            MDC.remove("scenario");
+        }
     }
 }
