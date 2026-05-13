b04b25e348294b3f6f6f3c70663a578b76b07d44
mmancini95 <matteo.mancini@pagopa.it>
2026-05-13 15:40:37 +0200

    feat: [QA-13946] aggiunto OneTrustBanner nell'interfaccia Sidebar

--- send/src/test/java/it/pagopa/send/Sidebar.java
+++ send/src/test/java/it/pagopa/send/Sidebar.java
@@ -3,6 +3,9 @@ package it.pagopa.send;
 import it.frontend.e2e.framework.annotation.selector.XPath;
 import it.frontend.e2e.framework.core.capability.core.Clickable;
 import it.frontend.e2e.framework.web.domain.Component;
+import it.pagopa.send.steps.login.component.OneTrustBanner;
+
+import java.util.Optional;
 
 public interface Sidebar extends Component {
 
@@ -18,7 +21,10 @@ public interface Sidebar extends Component {
     @XPath("//*[@id=\"side-item-Platform status\"]")
     Clickable platformStatus();
 
+    Optional<OneTrustBanner> oneTrustBanner();
+
     default void goToSection(String section) {
+        oneTrustBanner().ifPresent(OneTrustBanner::accept);
         switch (section) {
             case "Notifications" -> notifications().click();
             case "API Key" -> apiKeys().click();
