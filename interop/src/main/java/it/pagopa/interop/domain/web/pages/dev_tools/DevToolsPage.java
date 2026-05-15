b2c5badd5c3df0c4cd1fd388c4e4ef342a859826
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-15 12:24:45 +0200

    feat(/interop): add SimulatoreOttenimentoTokenSection to DevToolsPage and update section assertions

--- interop/src/main/java/it/pagopa/interop/domain/web/pages/dev_tools/DevToolsPage.java
+++ interop/src/main/java/it/pagopa/interop/domain/web/pages/dev_tools/DevToolsPage.java
@@ -5,6 +5,7 @@ import it.frontend.e2e.framework.annotation.selector.XPath;
 import it.frontend.e2e.framework.web.capability.core.Readable;
 import it.frontend.e2e.framework.web.domain.Page;
 import it.pagopa.interop.domain.web.pages.dev_tools.components.DebugClientAssertionSection;
+import it.pagopa.interop.domain.web.pages.dev_tools.components.SimulatoreOttenimentoTokenSection;
 import org.assertj.core.api.Assertions;
 
 @Url("${interop.web.base-url}/tool-sviluppo")
@@ -16,8 +17,11 @@ public interface DevToolsPage extends Page {
     @XPath("//*[@id=\"interop-sidenav-main\"]/div/main/div/div[2]/div/div/p")
     Readable<String> pageSubtitle();
 
-    @XPath("//*[@id=\"interop-sidenav-main\"]/div/main/div/div[3]/div/div[2]/section")
+    @XPath("//*[@id=\"interop-sidenav-main\"]/div/main/div/div[3]/div/div[3]/section")
     DebugClientAssertionSection debugClientAssertionSection();
+
+    @XPath("//*[@id=\"interop-sidenav-main\"]/div/main/div/div[3]/div/div[2]/section")
+    SimulatoreOttenimentoTokenSection simulatoreOttenimentoTokenSection();
 
     @Override
     default void assertLoaded() {
@@ -26,8 +30,9 @@ public interface DevToolsPage extends Page {
                         .containsIgnoringCase("Tool per lo sviluppo")
         );
 
-        pageSubtitle().readAndAssert("Una sezione che contiene alcuni strumenti utili a supporto dello sviluppo.");
+        pageSubtitle().readAndAssert("Una sezione che contiene alcuni strumenti utili a supporto dello sviluppo");
 
         debugClientAssertionSection().assertLoaded();
+        simulatoreOttenimentoTokenSection().assertLoaded();
     }
 }
