a5862150014077b6de944c563bce7c936468edfe
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-15 12:24:49 +0200

    feat(/interop): add getSnackbarErrorMessage to WebBrowserService interface

--- interop/src/main/java/it/pagopa/interop/domain/services/browser/WebBrowserService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/browser/WebBrowserService.java
@@ -3,4 +3,5 @@ package it.pagopa.interop.domain.services.browser;
 public interface WebBrowserService {
     boolean hasSessionToken();
     void setSessionToken(String token);
+    String getSnackbarErrorMessage();
 }
