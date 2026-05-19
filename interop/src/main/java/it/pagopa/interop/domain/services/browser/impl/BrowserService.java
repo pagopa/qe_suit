6d4640267a03760adff8ea373afceb5ed7a7e505
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-20 01:59:52 +0200

    feat: aggiunge login centralizzato e gestione stato utente su BrowserService tramite BrowserContext

--- interop/src/main/java/it/pagopa/interop/domain/services/browser/impl/BrowserService.java
+++ interop/src/main/java/it/pagopa/interop/domain/services/browser/impl/BrowserService.java
@@ -2,8 +2,12 @@ package it.pagopa.interop.domain.services.browser.impl;
 
 import it.frontend.e2e.framework.web.WebPresentationGateway;
 import it.frontend.e2e.framework.web.model.location.Url;
+import it.pagopa.interop.domain.context.BrowserContext;
+import it.pagopa.interop.domain.enums.Tenant;
+import it.pagopa.interop.domain.enums.User;
 import it.pagopa.interop.domain.services.browser.WebBrowserService;
 import it.pagopa.interop.domain.web.commons.component.Snackbar;
+import it.pagopa.interop.domain.web.commons.page.login.LoginPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +18,8 @@ import org.springframework.stereotype.Service;
 public class BrowserService implements WebBrowserService {
 
     private final WebPresentationGateway webPresentationGateway;
+    private final LoginPage loginPage;
+    private final BrowserContext browserContext;
 
     @Value("${interop.web.base-url}")
     private String baseUrl;
@@ -40,10 +46,23 @@ public class BrowserService implements WebBrowserService {
     }
 
     @Override
-    public String getSnackbarErrorMessage(){
+    public void login(User user, Tenant tenant) {
+        if (isLoggedIn(user, tenant)) return;
+        loginPage.navigateTo();
+        loginPage.login(user, tenant);
+        browserContext.set(user, tenant);
+    }
+
+    @Override
+    public boolean isLoggedIn(User user, Tenant tenant) {
+        return browserContext.isLoggedIn(user, tenant);
+    }
+
+    @Override
+    public String getSnackbarErrorMessage() {
         Snackbar snackbar = webPresentationGateway.bind(Snackbar.class);
 
-        if(!snackbar.alert().isError())
+        if (!snackbar.alert().isError())
             throw new IllegalStateException("Snackbar non in stato di errore, impossibile leggere il messaggio");
 
         return snackbar.alert().message().read();
