fdade7fc5a35b69fc5392a54e28a98b0aee18160
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-20 01:59:59 +0200

    refactor: aggiorna import di CurrentUserContext al nuovo package domain.context in ClientController

--- interop/src/main/java/it/pagopa/interop/controller/ClientController.java
+++ interop/src/main/java/it/pagopa/interop/controller/ClientController.java
@@ -7,7 +7,7 @@ import it.pagopa.interop.domain.enums.User;
 import it.pagopa.interop.domain.model.Client;
 import it.pagopa.interop.domain.model.Purpose;
 import it.pagopa.interop.domain.services.client.ClientService;
-import it.pagopa.interop.infrastructure.client.auth.context.user.CurrentUserContext;
+import it.pagopa.interop.domain.context.CurrentUserContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
--- interop/src/main/java/it/pagopa/interop/infrastructure/client/auth/context/user/CurrentUser.java
+++
@@ -1,9 +0,0 @@
-package it.pagopa.interop.infrastructure.client.auth.context.user;
-
-public record CurrentUser(
-        String uid,
-        String organizationId,
-        String selfcareId,
-        String userRole
-) {
-}
