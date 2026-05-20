cd28bd2706d4309ed02d6c26fcd8d99139779b2f
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-20 02:00:41 +0200

    feat: aggiunge bean LoginPage a WebConfig per supportare l'iniezione tramite WebPresentationGateway

--- interop/src/main/java/it/pagopa/interop/domain/web/config/WebConfig.java
+++ interop/src/main/java/it/pagopa/interop/domain/web/config/WebConfig.java
@@ -6,6 +6,7 @@ import it.frontend.e2e.framework.web.adapter.model.BrowserSettings;
 import it.frontend.e2e.framework.web.adapter.selenium.SeleniumApiAdapter;
 import it.frontend.e2e.framework.web.config.WebSuiteBuilder;
 import it.frontend.e2e.framework.web.model.location.Url;
+import it.pagopa.interop.domain.web.commons.page.login.LoginPage;
 import it.pagopa.interop.domain.web.pages.dev_tools.DevToolsPage;
 import it.pagopa.interop.domain.web.pages.dev_tools.debug_client_assertion.DebugClientAssertionPage;
 import lombok.Getter;
@@ -40,6 +41,12 @@ public class WebConfig {
                     return Url.of(resolvedUrl);
                 })
                 .build();
+    }
+
+    @Bean
+    @ScenarioScope
+    public LoginPage loginPage(WebPresentationGateway webPresentationGateway) {
+        return webPresentationGateway.bind(LoginPage.class);
     }
 
     @Bean
