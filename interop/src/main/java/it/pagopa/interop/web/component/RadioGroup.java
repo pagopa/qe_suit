49c346eb431659c0e5049a1290a2264c9441fb9a
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-28 18:34:22 +0200

    feat: aggiunge componente RadioGroup per gestione gruppi di radio button MUI

---
+++ interop/src/main/java/it/pagopa/interop/web/component/RadioGroup.java
@@ -0,0 +1,29 @@
+package it.pagopa.interop.web.component;
+
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.web.domain.Component;
+
+import java.util.List;
+
+@XPath(".//div[contains(@class, 'MuiRadioGroup-root')]")
+public interface RadioGroup extends Component {
+
+    List<RadioButton> radioButtons();
+
+    default void select(String value){
+        radioButtons().stream()
+                .filter(radioButton -> radioButton.getLabel().equals(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Radio button with value " + value + " not found"))
+                .select();
+    }
+
+    default void selectLike(String value){
+        radioButtons().stream()
+                .filter(radioButton -> radioButton.getLabel().toLowerCase().contains(value.toLowerCase()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Radio button with value " + value + " not found"))
+                .select();
+    }
+
+}
