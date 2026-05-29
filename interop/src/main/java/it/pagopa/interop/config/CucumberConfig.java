cda455ab0654d51d7be82b7aa0afe1b833d10855
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-05-29 16:29:49 +0200

    feat: gestisce la trasformazione automatica di "$blank()" in stringa vuota nelle tabelle Cucumber

--- interop/src/main/java/it/pagopa/interop/config/CucumberConfig.java
+++ interop/src/main/java/it/pagopa/interop/config/CucumberConfig.java
@@ -4,14 +4,22 @@ import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.DefaultDataTableEntryTransformer;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CucumberConfig {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    // Questo trasforma AUTOMATICAMENTE qualsiasi tabella nel rispettivo Bean usando la reflection
     @DefaultDataTableEntryTransformer
     public Object transform(Map<String, String> entry, Type toValueType) {
-        return objectMapper.convertValue(entry, objectMapper.constructType(toValueType));
+        // 1. Creiamo una copia mutabile della mappa (Cucumber potrebbe passarla come immutabile)
+        Map<String, String> modifiableEntry = new HashMap<>(entry);
+
+        //TODO: da sostituire con l'integrazione di uno strategy
+        // 2. Se il valore è esattamente "$blank()", lo trasformiamo in una vera stringa vuota ""
+        modifiableEntry.replaceAll((key, value) -> "$blank()".equals(value) ? "" : value);
+
+        // 3. Jackson fa lo zapping finale sull'oggetto
+        return objectMapper.convertValue(modifiableEntry, objectMapper.constructType(toValueType));
     }
 }
