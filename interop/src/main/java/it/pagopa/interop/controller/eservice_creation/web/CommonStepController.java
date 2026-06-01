6d200beaa58fa485303f7675d20c3ec665819750
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-01 12:18:08 +0200

    refactor: semplifica i passaggi di navigazione e modifica i messaggi per una maggiore chiarezza

--- interop/src/main/java/it/pagopa/interop/controller/eservice_creation/web/CommonStepController.java
+++ interop/src/main/java/it/pagopa/interop/controller/eservice_creation/web/CommonStepController.java
@@ -1,6 +1,7 @@
 package it.pagopa.interop.controller.eservice_creation.web;
 
 import io.cucumber.java.en.When;
+import it.frontend.e2e.framework.web.domain.Component;
 import it.pagopa.interop.web.pages.eservice_creation.EServiceCreationPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,14 @@ public class CommonStepController {
 
     private final EServiceCreationPage eServiceCreationPage;
 
-    @When("l'utente clicca sul button 'Salva bozza e prosegui'")
+    @When("clicca sul button 'Salva bozza e prosegui'")
     public void saveDraft() {
         eServiceCreationPage.saveDraft();
+    }
+
+
+    public void assertStepLoaded(Component currentStep) {
+        currentStep.assertLoaded();
     }
 
 }
