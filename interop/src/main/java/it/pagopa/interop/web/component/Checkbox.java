d9c7d6d3b3965fc7c10a5d7ce4e1d13e537d40bb
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-01 14:45:49 +0200

    feat: aggiunge interfaccia Checkbox con metodi per la gestione dello stato e dei messaggi di errore

---
+++ interop/src/main/java/it/pagopa/interop/web/component/Checkbox.java
@@ -0,0 +1,61 @@
+package it.pagopa.interop.web.component;
+
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.core.capability.core.Clickable;
+import it.frontend.e2e.framework.web.capability.core.Readable;
+import it.frontend.e2e.framework.web.domain.Component;
+import it.frontend.e2e.framework.web.model.WebPresentationElement;
+
+import java.util.List;
+
+@XPath(".//span[contains(@class, 'MuiCheckbox-root')]")
+public interface Checkbox extends Component, Clickable {
+    String ERROR_CLASS = "Mui-error";
+    String CHECKED_CLASS = "Mui-checked";
+
+    @XPath(".//following::span[contains(@class, 'MuiFormControlLabel-label')]")
+    Readable<String> description();
+
+    @XPath(".//following::span[contains(@class, 'MuiFormHelperText-root')]")
+    FormHelperText helperText();
+
+    default void setChecked(boolean targetState) {
+        if (isChecked() != targetState) {
+            click();
+        }
+    }
+
+    default void check() {
+        setChecked(true);
+    }
+
+    default void uncheck() {
+        setChecked(false);
+    }
+
+    default String getHelperText(String labelId) {
+        return helperText().getAll()
+                .orElse(List.of()).stream()
+                .filter(we -> !we.getClasses().contains(ERROR_CLASS))
+                .filter(we -> labelId.equals(we.getAttributes().get("id")))
+                .findFirst()
+                .map(WebPresentationElement::getText)
+                .orElse("");
+    }
+
+    default String getErrorMessage(String labelId) {
+        return helperText().getAll()
+                .orElse(List.of()).stream()
+                .filter(we -> we.getClasses().contains(ERROR_CLASS))
+                .filter(we -> labelId.equals(we.getAttributes().get("id")))
+                .findFirst()
+                .map(WebPresentationElement::getText)
+                .orElse("");
+    }
+
+    default boolean isChecked() {
+        return helperText().getAll()
+                .map(list -> list.stream().anyMatch(we -> we.getClasses().contains(CHECKED_CLASS)))
+                .orElse(false);
+    }
+}
