a7e72ead0ac7283818fe805a71283a754cdfdc7e
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-01 17:23:04 +0200

    feat: rename WebDevToolsService to DevToolsService and update references in DevToolsController

--- interop/src/main/java/it/pagopa/interop/ui/service/DevToolsService.java
+++ interop/src/main/java/it/pagopa/interop/ui/service/DevToolsService.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class WebDevToolsService {
+public class DevToolsService {
 
     @Value("${interop.auth.client-assertion.grant_type}")
     private String clientAssertionGrantType;
