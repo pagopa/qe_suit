f31b9b53420c28c31661b89e7a7b3b34a6e6133f
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-01 12:25:14 +0200

    feat: aggiunge supporto per i passaggi di creazione e-service e definisce nuovi step nel controller

--- interop/src/main/java/it/pagopa/interop/controller/eservice_creation/web/CommonStepController.java
+++ interop/src/main/java/it/pagopa/interop/controller/eservice_creation/web/CommonStepController.java
@@ -1,5 +1,6 @@
 package it.pagopa.interop.controller.eservice_creation.web;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import it.frontend.e2e.framework.web.domain.Component;
 import it.pagopa.interop.web.pages.eservice_creation.EServiceCreationPage;
@@ -11,14 +12,13 @@ public class CommonStepController {
 
     private final EServiceCreationPage eServiceCreationPage;
 
-    @When("clicca sul button 'Salva bozza e prosegui'")
-    public void saveDraft() {
-        eServiceCreationPage.saveDraft();
-    }
-
-
+    @Given("si trova allo step {eServiceCreationStep}")
     public void assertStepLoaded(Component currentStep) {
         currentStep.assertLoaded();
     }
 
+    @When("clicca sul button 'Salva bozza e prosegui'")
+    public void saveDraft() {
+        eServiceCreationPage.saveDraft();
+    }
 }
---
+++ interop/src/main/resources/features/ui/create_eservice/create_async_eservice_technical_specification.feature
@@ -0,0 +1,9 @@
+Feature:
+  Come utente
+
+  Scenario: [CREATE_ASYNC_ESERVICE_TECHNICAL_SPECIFICATION_1]
+    Given un admin del Comune di Milano si trova alla pagina Creazione EService del portale Interop
+    And compila lo step 'Informazioni generali' con i valori di default
+    And clicca sul button 'Salva bozza e prosegui'
+    And si trova allo step Specifiche tecniche
+
