9c0dba099515665a483228e0aaac3d9e9dd72ebd
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-03 18:23:01 +0200

    feat: enhance ContextHandler and resource interfaces for improved context management and HTTP response handling

--- interop/src/main/java/it/pagopa/interop/bff/service/template/ContextHandler.java
+++ interop/src/main/java/it/pagopa/interop/bff/service/template/ContextHandler.java
@@ -1,5 +1,8 @@
 package it.pagopa.interop.bff.service.template;
 
+import org.springframework.http.ResponseEntity;
+
 public interface ContextHandler<Entity> {
-    void updateContext(Entity entity);
+    void doUpdateModelContext(Entity model);
+    void doUpdateHttpContext(ResponseEntity<Entity> entity);
 }
