968db2fcd81b31915723e14d3ddee77bf7c0d55e
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-03 17:30:21 +0200

    feat: add Dialog interface with XPath annotations for confirmation button

---
+++ interop/src/main/java/it/pagopa/interop/ui/component/Dialog.java
@@ -0,0 +1,10 @@
+package it.pagopa.interop.ui.component;
+
+import it.frontend.e2e.framework.annotation.selector.XPath;
+import it.frontend.e2e.framework.web.domain.Component;
+
+@XPath(".//*[contains(@class, 'MuiDialog-paper')]")
+public interface Dialog extends Component {
+    @XPath(".//button[normalize-space()='Conferma']")
+    Button confirmBtn();
+}
