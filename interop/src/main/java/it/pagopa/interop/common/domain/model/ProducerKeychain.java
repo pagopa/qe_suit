5a9023ddeaabadee8a62c8c3d0190c1c2b97cabd
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-04 23:11:20 +0200

    feat: implement ProducerKeychain model, service, and controller for keychain management

---
+++ interop/src/main/java/it/pagopa/interop/common/domain/model/ProducerKeychain.java
@@ -0,0 +1,7 @@
+package it.pagopa.interop.common.domain.model;
+
+import java.util.UUID;
+
+public class ProducerKeychain extends it.pagopa.interop.generated.openapi.clients.bff.model.ProducerKeychain implements TestModel<UUID> {
+
+}
