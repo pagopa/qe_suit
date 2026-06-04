817d07bb4660bdc6b8e563212bb47e21ed435f60
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-04 23:57:11 +0200

    refactor: streamline setupEservice methods for improved readability

--- interop/src/main/java/it/pagopa/interop/bff/controller/EserviceController.java
+++ interop/src/main/java/it/pagopa/interop/bff/controller/EserviceController.java
@@ -16,29 +16,29 @@ public class EserviceController {
     @Given("un eservice creato da {tenant} con una richiesta di fruizione associata da {tenant}")
     public void setupEsericeAndAgreement(Tenant producer, Tenant consumer) {
         journey
-                .withProducer(producer, User.getTenantAdmin(producer))
-                .publishEservice()
-                .withConsumer(consumer, User.getTenantAdmin(consumer))
-                .addActiveAgreement();
+            .withProducer(producer, User.getTenantAdmin(producer))
+            .publishEservice()
+            .withConsumer(consumer, User.getTenantAdmin(consumer))
+            .addActiveAgreement();
     }
 
     @Given("un eservice creato da {tenant} con una richiesta di fruizione e una finalità associate da {tenant}")
     public void setupEservice(Tenant producer, Tenant consumer) {
         journey
-                .withProducer(producer, User.getTenantAdmin(producer))
-                .publishEservice()
-                .withConsumer(consumer, User.getTenantAdmin(consumer))
-                .addActiveAgreement()
-                .addPurposeInState(PurposeVersionState.ACTIVE);
+            .withProducer(producer, User.getTenantAdmin(producer))
+            .publishEservice()
+            .withConsumer(consumer, User.getTenantAdmin(consumer))
+            .addActiveAgreement()
+            .addPurposeInState(PurposeVersionState.ACTIVE);
     }
 
     @Given("un eservice creato da {tenant} con una richiesta di fruizione e una finalità in stato {purposeState} provenienti da {tenant}")
     public void setupEservice(Tenant producer, PurposeVersionState purposeState, Tenant consumer) {
         journey
-                .withProducer(producer, User.getTenantAdmin(producer))
-                .publishEservice()
-                .withConsumer(consumer, User.getTenantAdmin(consumer))
-                .addActiveAgreement()
-                .addPurposeInState(purposeState);
+            .withProducer(producer, User.getTenantAdmin(producer))
+            .publishEservice()
+            .withConsumer(consumer, User.getTenantAdmin(consumer))
+            .addActiveAgreement()
+            .addPurposeInState(purposeState);
     }
 }
