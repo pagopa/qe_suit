1ea1569c460a87b79b38ad0e1e24fc8fabc870ac
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-12 22:54:30 +0200

    feat: refactor parameter types and mappers to implement new models and improve context handling

--- interop/src/main/java/it/pagopa/interop/common/cucumber/parameter_type/JwtParameterType.java
+++ interop/src/main/java/it/pagopa/interop/common/cucumber/parameter_type/JwtParameterType.java
@@ -1,12 +1,7 @@
 package it.pagopa.interop.common.cucumber.parameter_type;
 
-import io.cucumber.datatable.DataTable;
 import io.cucumber.java.DataTableType;
 import io.cucumber.java.ParameterType;
-import it.pagopa.interop.common.cucumber.context.ScenarioContext;
-import it.pagopa.interop.common.cucumber.parameter_type.mapper.ClientAssertionValidationResultMapper;
-import it.pagopa.interop.common.domain.model.ClientAssertion;
-import it.pagopa.interop.common.domain.model.VoucherRequestValidationResult;
 import it.pagopa.interop.common.utils.JwtBuilderUtils;
 import it.pagopa.interop.common.utils.KeyPairUtils;
 import lombok.RequiredArgsConstructor;
@@ -15,24 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Map;
 
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class ClientAssertionParameterType {
-
-    private final ScenarioContext scenarioContext;
-    private final ClientAssertionValidationResultMapper clientAssertionValidationResultMapper;
-
-    @ParameterType("client assertion|client assertion creata")
-    public ClientAssertion currentClientAssertion(String token) {
-        return scenarioContext.getLastOrThrow(ClientAssertion.class);
-    }
+public class JwtParameterType {
 
     @ParameterType("RSA|EC|ED25519")
     public KeyPairUtils.KeyAlgorithm keyAlgorithm(String alg) {
         return KeyPairUtils.KeyAlgorithm.valueOf(alg);
-    }
-
-    @DataTableType
-    public VoucherRequestValidationResult fromDataTable(DataTable dataTable) {
-        return clientAssertionValidationResultMapper.fromDataTable(dataTable);
     }
 
     @DataTableType
---
+++ interop/src/main/java/it/pagopa/interop/common/cucumber/parameter_type/VoucherParameterType.java
@@ -0,0 +1,19 @@
+package it.pagopa.interop.common.cucumber.parameter_type;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.DataTableType;
+import it.pagopa.interop.common.cucumber.parameter_type.mapper.VoucherRequestValidationResultMapper;
+import it.pagopa.interop.common.domain.model.VoucherRequestValidationResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class VoucherParameterType {
+
+    private final VoucherRequestValidationResultMapper voucherRequestValidationResultMapper;
+
+    @DataTableType
+    public VoucherRequestValidationResult fromDataTable(DataTable dataTable) {
+        return voucherRequestValidationResultMapper.fromDataTable(dataTable);
+    }
+}
