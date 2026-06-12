8f73a4a76f4f4f145a707962536d5affd9c38a02
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-12 21:57:40 +0200

    feat: refactor Purpose class to implement TestModel and update context references to ScenarioContext

--- interop/src/main/java/it/pagopa/interop/common/domain/model/Purpose.java
+++ interop/src/main/java/it/pagopa/interop/common/domain/model/Purpose.java
@@ -4,12 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.Delegate;
 
 @RequiredArgsConstructor
-public class Purpose extends AbstractModel{
+public class Purpose implements TestModel {
     @Delegate
     private final it.pagopa.interop.generated.openapi.clients.bff.model.Purpose embeddedModel;
-
-    @Override
-    public String getUniqueIdentifier() {
-        return getId().toString();
-    }
 }
