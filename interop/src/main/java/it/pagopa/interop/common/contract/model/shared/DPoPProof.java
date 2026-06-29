d612f651beeac242ba23b925916d3cdc90374465
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-29 17:30:56 +0200

    feat: enhance DPoPProof with UUID generation and refactor key handling in DPoPProofController

--- interop/src/main/java/it/pagopa/interop/common/contract/model/shared/DPoPProof.java
+++ interop/src/main/java/it/pagopa/interop/common/contract/model/shared/DPoPProof.java
@@ -1,13 +1,17 @@
 package it.pagopa.interop.common.contract.model.shared;
 
+import it.pagopa.interop.common.contract.model.TestModel;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
+import java.util.UUID;
+
 @Value
 @Builder(toBuilder = true)
 @Jacksonized
-public class DPoPProof {
+public class DPoPProof implements TestModel {
     String jwt;
     Key key;
+    UUID id = UUID.randomUUID();
 }
