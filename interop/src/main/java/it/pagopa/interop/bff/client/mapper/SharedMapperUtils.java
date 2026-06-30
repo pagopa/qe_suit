d4fbe8ebbf54e45e940695f910a229c12aaf962f
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-30 20:44:48 +0200

    feat: add SharedMapperUtils with utility method for mapping strings to Instants

---
+++ interop/src/main/java/it/pagopa/interop/bff/client/mapper/SharedMapperUtils.java
@@ -0,0 +1,16 @@
+package it.pagopa.interop.bff.client.mapper;
+
+import org.mapstruct.Named;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+
+public class SharedMapperUtils {
+
+    @Named("mapStringToInstant")
+    public Instant mapStringToInstant(String dateStr) {
+        if (dateStr == null || dateStr.isBlank()) {
+            return null;
+        }
+        return OffsetDateTime.parse(dateStr).toInstant();
+    }
+}
