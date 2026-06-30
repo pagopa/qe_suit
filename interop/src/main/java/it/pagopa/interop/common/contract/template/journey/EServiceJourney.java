57bcc47937bddf218e139fdd3b518a1305f0c4cc
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-06-30 20:44:44 +0200

    feat: refactor service package structure and update imports for consistency

--- interop/src/main/java/it/pagopa/interop/common/contract/template/journey/EServiceJourney.java
+++
@@ -1,6 +0,0 @@
-package it.pagopa.interop.common.contract.template.journey;
-
-public interface EServiceJourney<SELF extends EServiceJourney<SELF>> {
-    SELF createDraftEService();
-    SELF publishEservice();
-}
