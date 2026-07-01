aa75dfbbe09fecf51fc4cedd459aa5deef5c30ea
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-01 18:21:21 +0200

    feat: simplify IBaseJourney interface and update TestJourney implementation

--- interop/src/main/java/it/pagopa/interop/common/contract/journey/IBaseJourney.java
+++ interop/src/main/java/it/pagopa/interop/common/contract/journey/IBaseJourney.java
@@ -3,8 +3,7 @@ package it.pagopa.interop.common.contract.journey;
 import it.pagopa.interop.common.contract.model.shared.enums.Tenant;
 import it.pagopa.interop.common.contract.model.shared.enums.User;
 
-public interface IBaseJourney<SELF extends IBaseJourney<SELF>> extends
-        IEServiceJourney<SELF>, IAgreementJourney<SELF>, IClientJourney<SELF>, IPurposeJourney<SELF> {
+public interface IBaseJourney<SELF extends IBaseJourney<SELF>> {
 
     SELF withProducer(Tenant tenant, User user);
 
