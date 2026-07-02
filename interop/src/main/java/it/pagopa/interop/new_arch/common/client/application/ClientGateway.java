c7d231a390703f2912a530e75bb5b6d8e0aabb14
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-03 01:49:55 +0200

    refactor: update ClientGateway interface for improved client creation and user management

--- interop/src/main/java/it/pagopa/interop/new_arch/common/client/application/ClientGateway.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/common/client/application/ClientGateway.java
@@ -6,16 +6,23 @@ import it.pagopa.interop.new_arch.common.client.domain.ClientRef;
 import it.pagopa.interop.new_arch.common.infrastructure.security.Key;
 import it.pagopa.interop.new_arch.common.kernel.domain.User;
 import it.pagopa.interop.new_arch.common.purpose.domain.Purpose;
+import jakarta.annotation.Nullable;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClientGateway {
     Client getClient(ClientRef ref);
 
-    // CreationRequest per rendere configurabile la creazione con le chiavi o gli utenti o la purpose
-    Client createClient(ClientKind kind, List<User> members);
+    ClientRef createClient(ClientKind kind, @Nullable List<User> members);
 
-    Client addKey(Client client, Key key);
+    Optional<ClientRef> addKey(Client client, Key key);
 
-    Client addPurpose(Client client, Purpose purpose);
+    Optional<ClientRef> addUsersToClient(Client client, List<User> users);
+
+    default Optional<ClientRef> addUserToClient(Client client, User user) {
+        return addUsersToClient(client, List.of(user));
+    }
+
+    Optional<ClientRef> addPurpose(Client client, Purpose purpose);
 }
