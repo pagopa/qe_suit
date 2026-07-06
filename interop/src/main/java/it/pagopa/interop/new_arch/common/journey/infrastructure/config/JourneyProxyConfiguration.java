335c8b1796f73a76b0bb4dde5a158c85972454a7
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-07 00:43:16 +0200

    refactor: update JourneyProxyConfiguration to use InteropJourney and JourneyModule

--- interop/src/main/java/it/pagopa/interop/new_arch/common/journey/infrastructure/config/JourneyProxyConfiguration.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/common/journey/infrastructure/config/JourneyProxyConfiguration.java
@@ -1,10 +1,7 @@
 package it.pagopa.interop.new_arch.common.journey.infrastructure.config;
 
-import it.pagopa.interop.new_arch.common.journey.application.Journey;
-import it.pagopa.interop.new_arch.common.journey.application.AgreementJourney;
-import it.pagopa.interop.new_arch.common.journey.application.UserJourney;
-import it.pagopa.interop.new_arch.common.journey.infrastructure.AgreementJourneyImpl;
-import it.pagopa.interop.new_arch.common.journey.infrastructure.UserJourneyImpl;
+import it.pagopa.interop.new_arch.common.journey.application.InteropJourney;
+import it.pagopa.interop.new_arch.common.journey.application.JourneyModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,17 +12,11 @@ import java.util.List;
 public class JourneyProxyConfiguration {
 
     @Bean
-    @SuppressWarnings("unchecked")
-    public Journey<?> interopJourney(UserJourneyImpl userJourney, AgreementJourneyImpl agreementJourney) {
-
-        // Lista dei componenti reali che contengono le implementazioni
-        List<Object> delegates = List.of(userJourney, agreementJourney);
-
-        // Creiamo il proxy che unisce l'interfaccia globale e le sue sotto-interfacce
-        return (Journey<?>) Proxy.newProxyInstance(
-                Journey.class.getClassLoader(),
-                new Class<?>[]{Journey.class, UserJourney.class, AgreementJourney.class},
-                new JourneyInvocationHandler(delegates)
+    public InteropJourney interopJourney(List<JourneyModule> modules) {
+        return (InteropJourney) Proxy.newProxyInstance(
+                InteropJourney.class.getClassLoader(),
+                new Class<?>[]{InteropJourney.class},
+                new JourneyInvocationHandler(modules)
         );
     }
 }
