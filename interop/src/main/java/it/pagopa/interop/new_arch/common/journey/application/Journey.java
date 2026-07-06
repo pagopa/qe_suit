bb73fe1645df4854496fd9c5fd3f12d639032e35
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-07 00:43:02 +0200

    refactor: update CucumberConfig import to align with new architecture

--- interop/src/main/java/it/pagopa/interop/new_arch/common/journey/application/Journey.java
+++
@@ -1,5 +0,0 @@
-package it.pagopa.interop.new_arch.common.journey.application;
-
-public interface Journey<SELF extends Journey<SELF>> extends
-        AgreementJourney<SELF>, UserJourney<SELF> {
-}
