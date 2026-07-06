91a4d7094bd89d3328cfae2e749056366c8a7b2c
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-06 21:00:22 +0200

    refactor: enhance Finalizer interface with context and raw response methods

--- interop/src/main/java/it/pagopa/interop/new_arch/common/infrastructure/template/action/Finalizer.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/common/infrastructure/template/action/Finalizer.java
@@ -1,14 +1,29 @@
 package it.pagopa.interop.new_arch.common.infrastructure.template.action;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import it.pagopa.interop.new_arch.common.infrastructure.http.ApiResponse;
+import it.pagopa.interop.new_arch.common.infrastructure.template.action.context.BaseActionContext;
 import it.pagopa.interop.new_arch.common.kernel.domain.Identifiable;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public interface Finalizer<Response, Model extends Identifiable> {
-    ResponseEntity<Response> getResponse();
+    BaseActionContext getContext();
 
-    Model getModel();
+    ApiResponse getRawResponse();
 
-    List<Model> getModels();
+    @SuppressWarnings("unchecked")
+    default Response getResponse(){
+        return (Response) getRawResponse().as(getContext().getResponseClass());
+    }
+
+    @SuppressWarnings("unchecked")
+    default Model getModel(){
+        return (Model) getRawResponse().as(getContext().getModelClass());
+    }
+
+    default List<Model> getModels(){
+        return getRawResponse().as(new TypeReference<>() {
+        });
+    }
 }
