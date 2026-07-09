a7f1fc0ab86599b82c19a098c99e652d5d1e75ae
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-07-09 23:50:21 +0200

    refactor: extend ClientGateway interface to support Channel plugin functionality

--- interop/src/main/java/it/pagopa/interop/new_arch/common/client/application/ClientGateway.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/common/client/application/ClientGateway.java
@@ -4,13 +4,15 @@ import it.pagopa.interop.new_arch.common.client.domain.Client;
 import it.pagopa.interop.new_arch.common.client.domain.ClientKind;
 import it.pagopa.interop.new_arch.common.client.domain.ClientRef;
 import it.pagopa.interop.new_arch.common.infrastructure.security.crypto.Key;
+import it.pagopa.interop.new_arch.common.kernel.domain.Channel;
 import it.pagopa.interop.new_arch.common.kernel.domain.User;
 import it.pagopa.interop.new_arch.common.purpose.domain.Purpose;
 import jakarta.annotation.Nullable;
+import org.springframework.plugin.core.Plugin;
 
 import java.util.List;
 
-public interface ClientGateway {
+public interface ClientGateway extends Plugin<Channel> {
     Client getClient(ClientRef ref);
 
     Client createClient(ClientKind kind, @Nullable List<User> members);
