3db30f5268097983173eb4528a0d89fb840a65bf
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-14 19:31:15 +0200

    refactor: introduce WebEServiceCreationParameterType and WebEServiceTechSpecParameterType for Cucumber parameter handling

---
+++ interop/src/main/java/it/pagopa/interop/new_arch/web/eservice/infrastructure/cucumber/WebEServiceCreationParameterType.java
@@ -0,0 +1,27 @@
+package it.pagopa.interop.new_arch.web.eservice.infrastructure.cucumber;
+
+import io.cucumber.java.ParameterType;
+import it.frontend.e2e.framework.web.domain.Component;
+import it.pagopa.interop.new_arch.web.eservice.infrastructure.suit.component.creation_wizard.GeneralDataWizard;
+import it.pagopa.interop.new_arch.web.eservice.infrastructure.suit.component.creation_wizard.ThresholdAndAttributeWizard;
+import it.pagopa.interop.new_arch.web.eservice.infrastructure.suit.component.creation_wizard.technical.TechnicalSpecWizard;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class WebEServiceCreationParameterType {
+
+    private final GeneralDataWizard generalDataWizardComponent;
+    private final TechnicalSpecWizard technicalSpecWizard;
+    private final ThresholdAndAttributeWizard thresholdAndAttributeWizard;
+
+    @ParameterType("Informazioni generali|Specifiche tecniche|Soglie e attributi")
+    public Component eServiceCreationStep(String stepName) {
+        return switch (stepName.toLowerCase()) {
+            case "informazioni generali", "dati generali" -> generalDataWizardComponent;
+            case "specifiche tecniche" -> technicalSpecWizard;
+            case "soglie e attributi" -> thresholdAndAttributeWizard;
+            default -> throw new IllegalArgumentException("Illegal eservice creation step name: " + stepName);
+        };
+    }
+}
