c9085d1fd77a092ac3ecf251f3efab289a6a9709
mmancini95 <matteo.mancini@pagopa.it>
2026-07-16 17:40:56 +0200

    feature: [QA-15573] introdotto MittenteNotificationDetailsPage e NotificationDetailsProxy per migliorare la gestione delle notifiche e la navigazione nel portale SEND

--- send/src/main/java/it/pagopa/send/domain/web/pages/mittente/MittenteNotificationDetailsPage.java
+++ send/src/main/java/it/pagopa/send/domain/web/pages/mittente/MittenteNotificationDetailsPage.java
@@ -4,13 +4,9 @@ import it.frontend.e2e.framework.annotation.location.web.Url;
 import it.frontend.e2e.framework.annotation.selector.XPath;
 import it.frontend.e2e.framework.core.capability.core.Clickable;
 import it.frontend.e2e.framework.web.capability.core.Readable;
+import it.frontend.e2e.framework.web.domain.Component;
 import it.pagopa.send.web.notification_details.infrastructure.suit.NotificationDetailsPage;
 import it.pagopa.send.web.notification_details.infrastructure.suit.component.NotificationStatusDrawer;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.AttachmentSection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.NotificationStatusSection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.NotificationSummarySection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.PaymentSection;
-import it.pagopa.infrastructure.suit.component.Chip;
 import org.assertj.core.api.Assertions;
 
 @Url("${url.notifiche.mittente.dashboard}#selfCareToken=${token.mittente}")
@@ -20,147 +16,78 @@ public interface MittenteNotificationDetailsPage extends NotificationDetailsPage
     Readable<String> breadcrumbs();
 
     @XPath("//*[@id=\"notificationsTable.body.row\"]/td[7]/button")
-    Readable<String> alertMessage();
+    Clickable seeDetailsButton();
 
-    @XPath("//div[@id=\"page-header-container\"]/parent::div")
-    interface MittenteNotificationSummarySection extends NotificationSummarySection {
-//        @XPath("header iun")
-        @XPath(".//h1")
+    interface NotificationSummarySection extends Component {
+        @XPath("header iun")
         Readable<String> iunHeader();
 
-//        @XPath("header protocol number")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("header protocol number")
         Readable<String> protocolNumberHeader();
 
-//        @XPath("protocol number value")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("protocol number value")
         Readable<String> protocolNumberValue();
 
-//        @XPath("header sender")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("header sender")
         Readable<String> senderHeader();
 
-//        @XPath("sender value")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("sender value")
         Readable<String> senderValue();
 
-//        @XPath("header recipient")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("header recipient")
         Readable<String> recipientHeader();
 
-//        @XPath("recipient value")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("recipient value")
         Readable<String> recipientValue();
 
-//        @XPath("open details sidebar")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("open details sidebar")
         Clickable openDetailsSidebarButton();
 
         NotificationStatusDrawer notificationStatusDrawer();
 
-        @Override
-        default void assertLoaded() {
-            iunHeader().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            protocolNumberHeader().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            protocolNumberValue().readAndAssert((v) -> {
-                Assertions.assertThat(v).isNotNull();
-            });
-            senderHeader().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            senderValue().readAndAssert((v) -> {
-                Assertions.assertThat(v).isNotNull();
-            });
-            recipientHeader().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            recipientValue().readAndAssert((v) -> {
-                Assertions.assertThat(v).isNotNull();
-            });
-        }
     }
 
-    interface MittentePaymentSection extends PaymentSection {
-//        @XPath("header notice code")
-        @XPath("//*[@id=\"title-of-page\"]")
+    interface PaymentSection extends Component {
+        @XPath("header notice code")
         Readable<String> noticeCodeHeader();
 
-//        @XPath("notice code value")
-        @XPath("//*[@id=\"title-of-page\"]")
+        @XPath("notice code value")
         Readable<String> noticeCodeValue();
     }
 
-    @XPath("//h2[@id=\"notification-detail-document-attached\"]/ancestor::div[.//button[@data-testid=\"cancelNotificationBtn\"]][1]")
-    interface MittenteAttachmentSection extends AttachmentSection {
-//        @XPath("header")
-        @XPath("//h2[@id=\"notification-detail-document-attached\"]")
+    interface AttachmentSection extends Component {
+        @XPath("header")
         Readable<String> header();
 
-//        @XPath("griglia contenuti")
-        @XPath("//div[@data-testid=\"notificationDetailDocuments\"]//button[@data-testid=\"documentButton\"]/div/span")
+        @XPath("griglia contenuti")
         Readable<String> attachmentGrid();
-
-        @Override
-        default void assertLoaded() {
-            header().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            attachmentGrid().readAndAssert((g) -> {
-                Assertions.assertThat(g).isNotNull();
-            });
-        }
     }
 
-    @XPath(".//div[@data-testid='NotificationDetailTimeline']")
-    interface MittenteNotificationStatusSection extends NotificationStatusSection {
-//        @XPath("header")
-        @Override
-        @XPath("//*[@id=\"title-of-page\"]")
+    interface NotificationStatusSection extends Component {
+        @XPath("header")
         Readable<String> header();
 
-        @Override
-        @XPath("//*[@data-testid='NotificationDetailTimeline']//div[contains(@class, 'MuiChip-root')]")
-        Chip statusChip();
+        @XPath("chip stato notifica")
+        Readable<String> notificationStatusChip();
 
-//        @XPath("dettaglio stato notifica")
-        @Override
-        @XPath("//*[@id=\"title-of-page\"]")
-        Readable<String> detailsMessage();
+        @XPath("dettaglio stato notifica")
+        Readable<String> detailsText();
 
-//        @XPath("griglia contenuti")
-        @Override
-        @XPath("//*[@id=\"title-of-page\"]")
-        Clickable detailsButton();
-
-        @Override
-        default void assertLoaded() {
-            header().readAndAssert((h) -> {
-                Assertions.assertThat(h).isNotNull();
-            });
-            statusChip().text().readAndAssert((t) -> {
-                Assertions.assertThat(t).isNotNull();
-            });
-            detailsMessage().readAndAssert((m) -> {
-                Assertions.assertThat(m).isNotNull();
-            });
-        }
+        @XPath("griglia contenuti")
+        Clickable details();
     }
 
     @Override
-    MittenteNotificationSummarySection notificationSummarySection();
+    NotificationSummarySection notificationSummarySection();
 
     @Override
-    MittentePaymentSection paymentSection();
+    PaymentSection paymentSection();
 
     @Override
-    MittenteAttachmentSection attachmentSection();
+    AttachmentSection attachmentSection();
 
     @Override
-    MittenteNotificationStatusSection notificationStatusSection();
+    NotificationStatusSection notificationStatusSection();
 
     @Override
     default void assertLoaded() {
--- send/src/main/java/it/pagopa/send/web/notification_details/infrastructure/NotificationDetailsProxy.java
+++ send/src/main/java/it/pagopa/send/web/notification_details/infrastructure/NotificationDetailsProxy.java
@@ -1,80 +1,66 @@
 package it.pagopa.send.web.notification_details.infrastructure;
 
-import it.pagopa.send.common.domain.UserType;
+import it.frontend.e2e.framework.web.domain.Component;
+import it.pagopa.send.common.kernel.domain.UserType;
 import it.pagopa.send.domain.web.pages.destinatario.pf.NotificationDetailsPFPage;
 import it.pagopa.send.domain.web.pages.destinatario.pf.NotificationPFPage;
 import it.pagopa.send.domain.web.pages.mittente.DashboardPage;
 import it.pagopa.send.domain.web.pages.mittente.MittenteNotificationDetailsPage;
 import it.pagopa.send.web.infrastructure.cucumber.WebBrowserContext;
 import it.pagopa.send.web.notification_details.infrastructure.suit.NotificationDetailsPage;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.AttachmentSection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.NotificationStatusSection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.NotificationSummarySection;
-import it.pagopa.send.web.notification_details.infrastructure.suit.section.PaymentSection;
-import it.pagopa.send.web.notification_search.infrastructure.suit.NotificationSearchPage;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Map;
 
 @org.springframework.stereotype.Component
 @RequiredArgsConstructor
-public class NotificationDetailsProxy implements NotificationDetailsPage, NotificationSearchPage {
+public class NotificationDetailsProxy implements NotificationDetailsPage {
     private final DashboardPage dashboardPage;
     private final MittenteNotificationDetailsPage mittenteNotificationDetailsPage;
     private final NotificationPFPage notificationPFPage;
     private final NotificationDetailsPFPage notificationDetailsPFPage;
     private final WebBrowserContext webBrowserContext;
 
-    @Override
     public void goToNotificationDetails() {
-        currentSearchPage().goToNotificationDetails();
+        switch (webBrowserContext.getUser().getType()) {
+            case PA -> dashboardPage.goToNotificationDetails();
+            case PF, PG -> notificationPFPage.goToNotificationDetails();
+        }
+
         webBrowserContext.setCurrentPage(this);
     }
 
-    @Override
-    public void searchNotification(Map<String, String> searchParams) {
-        currentSearchPage().searchNotification(searchParams);
-    }
-
-    private NotificationSearchPage currentSearchPage() {
-        return webBrowserContext.getCurrentUser().getType() == UserType.PA
-                ? dashboardPage
-                : notificationPFPage;
-    }
-
-    private NotificationDetailsPage currentDetailsPage() {
-        return webBrowserContext.getCurrentUser().getType() == UserType.PA
+    private NotificationDetailsPage current() {
+        return webBrowserContext.getUser().getType() == UserType.PA
                 ? mittenteNotificationDetailsPage
                 : notificationDetailsPFPage;
     }
 
     @Override
-    public NotificationSummarySection notificationSummarySection() {
-        return currentDetailsPage().notificationSummarySection();
+    public Component notificationSummarySection() {
+        return current().notificationSummarySection();
     }
 
     @Override
-    public PaymentSection paymentSection() {
-        return currentDetailsPage().paymentSection();
+    public Component paymentSection() {
+        return current().paymentSection();
     }
 
     @Override
-    public AttachmentSection attachmentSection() {
-        return currentDetailsPage().attachmentSection();
+    public Component attachmentSection() {
+        return current().attachmentSection();
     }
 
     @Override
-    public NotificationStatusSection notificationStatusSection() {
-        return currentDetailsPage().notificationStatusSection();
+    public Component notificationStatusSection() {
+        return current().notificationStatusSection();
     }
 
     @Override
     public void assertLoaded() {
-        currentDetailsPage().assertLoaded();
+        current().assertLoaded();
     }
 
     @Override
-    public void navigateTo(String... pathParams) {
-        currentDetailsPage().navigateTo();
+    public void navigateTo() {
+        current().navigateTo();
     }
 }
