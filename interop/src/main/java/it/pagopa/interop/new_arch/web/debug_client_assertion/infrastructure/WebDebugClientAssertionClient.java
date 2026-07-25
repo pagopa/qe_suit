0d15fb9c2554c7d94060b141416a8b6b1009792d
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-07-25 12:42:24 +0200

    refactor: rename BrowserDebugClientAssertionClient to WebDebugClientAssertionClient for consistency

--- interop/src/main/java/it/pagopa/interop/new_arch/web/debug_client_assertion/infrastructure/WebDebugClientAssertionClient.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/web/debug_client_assertion/infrastructure/WebDebugClientAssertionClient.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class BrowserDebugClientAssertionClient extends BrowserClient {
+public class WebDebugClientAssertionClient extends BrowserClient {
     @Value("${interop.auth.client-assertion.grant_type}")
     private String clientAssertionGrantType;
 
--- interop/src/main/java/it/pagopa/interop/new_arch/web/debug_client_assertion/infrastructure/WebDebugClientAssertionGateway.java
+++ interop/src/main/java/it/pagopa/interop/new_arch/web/debug_client_assertion/infrastructure/WebDebugClientAssertionGateway.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class WebDebugClientAssertionGateway implements DebugClientAssertionGateway {
 
-    private final BrowserDebugClientAssertionClient client;
+    private final WebDebugClientAssertionClient client;
 
     @Override
     public DebugClientAssertionValidation executeClientAssertionValidation(String clientAssertion, String dpopProof, ClientKind clientKind, String clientId) {
