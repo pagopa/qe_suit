d0be1f0073d796468c77bbe66d7e78d43741881e
Luigi Gjoni <luigi.gjoni@alten.it>
2026-07-31 10:37:53 +0200

    feat: [QA-15533] define EServiceDetailPage checks

--- interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/page/EServiceDetailPage.java
+++ interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/page/EServiceDetailPage.java
@@ -4,15 +4,77 @@ import it.frontend.e2e.framework.annotation.location.web.Url;
 import it.frontend.e2e.framework.annotation.selector.XPath;
 import it.frontend.e2e.framework.web.capability.core.Readable;
 import it.frontend.e2e.framework.web.domain.Page;
+import it.pagopa.interop.web.infrastructure.suit.component.Breadcrumb;
+import it.pagopa.interop.web.infrastructure.suit.component.Button;
+import org.assertj.core.api.SoftAssertions;
+
+import java.util.List;
 
 @Url("${interop.web.catalog}")
 public interface EServiceDetailPage extends Page {
 
-    @XPath(".//h1")
+    @XPath(".//h1[" +
+            "contains(concat(' ', normalize-space(@class), ' '), ' MuiTypography-root ') and " +
+            "contains(concat(' ', normalize-space(@class), ' '), ' MuiTypography-h4 ')" +
+            "]")
     Readable<String> eServiceName();
+
+    Breadcrumb breadcrumb();
+
+    // TODO valutare se può andare bene questo XPath o se sia troppo specifico e quindi esposto a cambiamenti lato FE
+    @XPath(".//button" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButtonBase-root ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-root ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-contained ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-containedPrimary ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-sizeSmall ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-containedSizeSmall ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-colorPrimary ')]" +
+            "[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-disableElevation ')]" +
+            "[.//span[contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-icon ')" +
+            " and contains(concat(' ', normalize-space(@class), ' '), ' MuiButton-startIcon ')]]" +
+            "[contains(normalize-space(.), 'Richiedi fruizione')" +
+            "]")
+    Button requestAgreementButton();
 
     @Override
     default void assertLoaded() {
-        Page.super.assertLoaded();
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(eServiceName().read())
+                    .as("Il nome dell'eservice non deve essere vuoto")
+                    .isNotEmpty();
+
+            softly.assertThat(requestAgreementButton().isDisabled())
+                    .as("Il bottone 'Richiedi Fruizione' non deve essere disabilitato")
+                    .isFalse();
+
+            softly.assertThat(isBreadcrumbCorrect())
+                    .as("Ordine o contenuto del breadcrumb non conforme al figma")
+                    .isTrue();
+        });
+    }
+
+    default boolean isBreadcrumbCorrect() {
+        boolean ret = true;
+        List<String> breadcrumbItems = this.breadcrumb().getBreadcrumbItems().stream()
+                .map(it.frontend.e2e.framework.core.capability.core.Readable::read)
+                .toList();
+        List<String> figmaBreadcrumbItems = List.of(
+                "Catalogo e-service",
+                "Visualizza e-service"
+        );
+
+        // check 1:1 tra
+        //      - elementi trovati da UI (breadCrumbItems) e
+        //      - desiderata imposta su Figma (figmaBreadcrumbItems)
+        for(int i=0; i<breadcrumbItems.size(); ++i) {
+            String breadcrumbItem = breadcrumbItems.get(i);
+            String uiBreadcrumbItem = figmaBreadcrumbItems.get(i);
+            if(breadcrumbItem.compareTo(uiBreadcrumbItem)!=0) {
+                ret = false;
+                break;
+            }
+        }
+        return ret;
     }
 }
--- interop/src/main/java/it/pagopa/interop/web/infrastructure/config/suit/component/Breadcrumb.java
+++ interop/src/main/java/it/pagopa/interop/web/infrastructure/config/suit/component/Breadcrumb.java
@@ -10,4 +10,13 @@ import java.util.List;
 public interface Breadcrumb extends Component {
     @XPath(".//li[not(contains(concat(' ', normalize-space(@class), ' '), ' MuiBreadcrumbs-separator '))]")
     List<Readable<String>> items();
+
+    default boolean exists(String item){
+        return items().stream()
+                .anyMatch(readableStr -> readableStr.read().compareTo(item) == 0);
+    }
+
+    default List<Readable<String>> getBreadcrumbItems(){
+        return this.items();
+    }
 }
