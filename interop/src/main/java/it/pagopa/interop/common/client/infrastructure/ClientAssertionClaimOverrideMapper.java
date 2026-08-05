2bd387284856658791f4f663400a335772c43896
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-08-05 10:39:57 +0200

    feat: [QA-16522] add ClientAssertionClaimOverride and related mapping for improved client assertion handling

---
+++ interop/src/main/java/it/pagopa/interop/common/client/infrastructure/ClientAssertionClaimOverrideMapper.java
@@ -0,0 +1,19 @@
+package it.pagopa.interop.common.client.infrastructure;
+
+import it.pagopa.interop.common.client.domain.ClientAssertionClaimOverride;
+import it.pagopa.interop.common.infrastructure.config.TestMapperConfig;
+import it.pagopa.interop.common.infrastructure.mapping.SharedMapperUtils;
+import it.pagopa.interop.common.infrastructure.security.jwt.JwtBuilder;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(config = TestMapperConfig.class, uses = { SharedMapperUtils.class })
+public interface ClientAssertionClaimOverrideMapper {
+
+    JwtBuilder.JwtClaimOverride map(ClientAssertionClaimOverride source);
+
+    default List<JwtBuilder.JwtClaimOverride> map(List<ClientAssertionClaimOverride> source) {
+        return source == null ? null : source.stream().map(this::map).toList();
+    }
+}
