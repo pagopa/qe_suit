7adab0c4def08bd25e6841b0c8007f5ae5ffe3bb
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-08-07 18:47:35 +0200

    feat: [QA-16634] add FinalizerJourney interface and implementation for entity retrieval

---
+++ interop/src/main/java/it/pagopa/interop/common/journey/infrastructure/FinalizerJourneyImpl.java
@@ -0,0 +1,19 @@
+package it.pagopa.interop.common.journey.infrastructure;
+
+import it.pagopa.interop.common.infrastructure.context.EntityStore;
+import it.pagopa.interop.common.journey.application.FinalizerJourney;
+import it.pagopa.interop.common.kernel.Identifiable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FinalizerJourneyImpl implements FinalizerJourney {
+
+    private final EntityStore entityStore;
+
+    @Override
+    public <T extends Identifiable> T get(Class<T> clazz) {
+        return entityStore.getLastOrThrow(clazz);
+    }
+}
