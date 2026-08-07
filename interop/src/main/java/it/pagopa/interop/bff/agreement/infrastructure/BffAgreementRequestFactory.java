aefbe774538f45ef678b70ee7f328858e09fc79c
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-08-07 18:48:06 +0200

    refactor: [QA-16634] remove unused dependencies and methods from BffAgreementRequestFactory

--- interop/src/main/java/it/pagopa/interop/bff/agreement/infrastructure/BffAgreementRequestFactory.java
+++ interop/src/main/java/it/pagopa/interop/bff/agreement/infrastructure/BffAgreementRequestFactory.java
@@ -1,13 +1,9 @@
 package it.pagopa.interop.bff.agreement.infrastructure;
 
-import it.pagopa.interop.generated.openapi.clients.bff.model.AgreementPayload;
 import it.pagopa.interop.common.eservice.domain.EService;
 import it.pagopa.interop.common.eservice.domain.EServiceDescriptor;
-import it.pagopa.interop.common.infrastructure.context.EntityStore;
-import it.pagopa.interop.common.journey.application.InteropJourney;
 import it.pagopa.interop.common.kernel.domain.Delegation;
-import it.pagopa.interop.common.kernel.domain.Tenant;
-import it.pagopa.interop.common.kernel.domain.UserRole;
+import it.pagopa.interop.generated.openapi.clients.bff.model.AgreementPayload;
 import lombok.RequiredArgsConstructor;
 import org.instancio.Instancio;
 import org.jspecify.annotations.Nullable;
@@ -19,9 +15,6 @@ import static org.instancio.Select.field;
 @RequiredArgsConstructor
 public class BffAgreementRequestFactory {
 
-    private final InteropJourney interopJourney;
-    private final EntityStore entityStore;
-
     public AgreementPayload creationRequest(EService eService, EServiceDescriptor descriptor, @Nullable Delegation delegation) {
         return Instancio.of(AgreementPayload.class)
                 .set(field(AgreementPayload::getEserviceId), eService.getId())
@@ -29,14 +22,4 @@ public class BffAgreementRequestFactory {
                 .set(field(AgreementPayload::getDelegationId), delegation != null ? delegation.getId() : null)
                 .create();
     }
-
-    public AgreementPayload baseCreationRequest() {
-        interopJourney
-                .withProducer(Tenant.COMUNE_DI_MILANO, UserRole.ADMIN)
-                .createEService();
-
-        final EService eService = entityStore.getLastOrThrow(EService.class);
-        return creationRequest(eService, eService.getLastDraftDescriptor(), null);
-    }
-
 }
