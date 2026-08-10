7a5698b73432522f52b01ec93d7658b362571a48
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-10 18:23:06 +0200

    refactor: [QA-16634] update BffAgreementContractTest to utilize HttpContract for API calls and streamline request handling

--- interop/src/main/java/it/pagopa/interop/common/infrastructure/http/contract/engine/RequestParts.java
+++
@@ -1,6 +0,0 @@
-package it.pagopa.interop.common.infrastructure.http.contract.engine;
-
-import java.util.Map;
-
-public record RequestParts(Map<String, Object> inputs, Map<String, Object> body) {
-}
