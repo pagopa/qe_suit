1290521e8a710c37e1533ad668b132e36d7e5e58
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-11 20:50:57 +0200

    feat: [QA-16522] add RandomUtils class for generating random alphanumeric names

---
+++ interop/src/main/java/it/pagopa/interop/common/infrastructure/utils/RandomUtils.java
@@ -0,0 +1,20 @@
+package it.pagopa.interop.common.infrastructure.utils;
+
+import org.instancio.Instancio;
+
+import static org.instancio.Select.allStrings;
+
+public class RandomUtils {
+    private RandomUtils() {
+    }
+
+    public static String randomAlphanumericName(String prefix,  int length) {
+        String randomPart = Instancio.of(String.class)
+                .generate(allStrings(), gen -> gen.string().prefix(prefix+"-").length(length)).create();
+        return prefix + randomPart;
+    }
+
+    public static String randomAlphanumericName(String prefix) {
+       return randomAlphanumericName(prefix, 15);
+    }
+}
