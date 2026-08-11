ecec8741829d96a8d0eb878e9a1ed35d98057003
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-11 22:00:13 +0200

    feat: [QA-16522] update ChannelContext and configuration for cucumber profile support

--- interop/src/main/java/it/pagopa/interop/common/infrastructure/context/ContextConfig.java
+++ interop/src/main/java/it/pagopa/interop/common/infrastructure/context/ContextConfig.java
@@ -5,8 +5,10 @@ import it.pagopa.interop.common.infrastructure.cucumber.context.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("cucumber")
 public class ContextConfig {
     @Bean
     @ScenarioScope
--- interop/src/main/java/it/pagopa/interop/common/infrastructure/cucumber/context/ChannelContext.java
+++ interop/src/main/java/it/pagopa/interop/common/infrastructure/cucumber/context/ChannelContext.java
@@ -1,16 +1,12 @@
 package it.pagopa.interop.common.infrastructure.cucumber.context;
 
-import io.cucumber.spring.ScenarioScope;
 import it.pagopa.interop.common.infrastructure.context.CurrentChannel;
 import it.pagopa.interop.common.kernel.domain.Channel;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
-@Component
 @Getter
 @Setter
-@ScenarioScope
 public class ChannelContext implements CurrentChannel {
     private Channel currentChannel;
 }
