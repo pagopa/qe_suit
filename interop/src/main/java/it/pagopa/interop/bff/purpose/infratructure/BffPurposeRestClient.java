5c7c19672e644d30e4b300acfc3532c39858fd28
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-27 10:28:53 +0200

    feat: [QA-16522] add methods for creating and managing purpose versions in BffPurposeRestClient

--- interop/src/main/java/it/pagopa/interop/bff/purpose/infratructure/BffPurposeRestClient.java
+++ interop/src/main/java/it/pagopa/interop/bff/purpose/infratructure/BffPurposeRestClient.java
@@ -1,9 +1,18 @@
 package it.pagopa.interop.bff.purpose.infratructure;
 
 import it.pagopa.interop.common.infrastructure.template.RestClient;
+import it.pagopa.interop.common.infrastructure.template.action.TestChain;
 import it.pagopa.interop.generated.openapi.clients.bff.ApiClient;
 import it.pagopa.interop.generated.openapi.clients.bff.api.PurposesApi;
+import it.pagopa.interop.generated.openapi.clients.bff.model.CreatedResource;
+import it.pagopa.interop.generated.openapi.clients.bff.model.DelegationRef;
+import it.pagopa.interop.generated.openapi.clients.bff.model.PurposeSeed;
+import it.pagopa.interop.generated.openapi.clients.bff.model.PurposeVersionResource;
+import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.function.Function;
 
 @Component
 public class BffPurposeRestClient extends RestClient {
@@ -12,5 +21,34 @@ public class BffPurposeRestClient extends RestClient {
 
     public BffPurposeRestClient(ApiClient apiClient) {
         this.purposesApi = apiClient.purposes();
+    }
+
+    public TestChain<CreatedResource> createPurpose(@Nonnull PurposeSeed payload) {
+        return execute(
+                () -> purposesApi.createPurpose().body(payload).execute(Function.identity()),
+                CreatedResource.class
+        );
+    }
+
+    public TestChain<PurposeVersionResource> activatePurposeVersion(@Nonnull UUID purposeId, @Nonnull UUID versionId, DelegationRef delegationRef) {
+        return execute(
+                () -> purposesApi.activatePurposeVersion()
+                        .purposeIdPath(purposeId)
+                        .versionIdPath(versionId)
+                        .body(delegationRef)
+                        .execute(Function.identity()),
+                PurposeVersionResource.class
+        );
+    }
+
+    public TestChain<PurposeVersionResource> suspendPurposeVersion(@Nonnull UUID purposeId, @Nonnull UUID versionId, DelegationRef delegationRef) {
+        return execute(
+                () -> purposesApi.suspendPurposeVersion()
+                        .purposeIdPath(purposeId)
+                        .versionIdPath(versionId)
+                        .body(delegationRef)
+                        .execute(Function.identity()),
+                PurposeVersionResource.class
+        );
     }
 }
