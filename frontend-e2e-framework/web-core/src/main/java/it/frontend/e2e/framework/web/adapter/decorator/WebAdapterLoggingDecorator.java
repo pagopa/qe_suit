1d4b0db589e6633161e0d6098a060cf7b7ace8ab
mmancini95 <matteo.mancini@pagopa.it>
2026-08-28 09:14:53 +0200

    feature: [QA-15573] implementate nuove azioni di login per Tenant e Recipient, aggiunta gestione sessionStorage al browser per forza la login

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/decorator/WebAdapterLoggingDecorator.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/decorator/WebAdapterLoggingDecorator.java
@@ -3,11 +3,9 @@ package it.frontend.e2e.framework.web.adapter.decorator;
 import it.frontend.e2e.framework.core.adapter.decorator.AbstractAdapterLoggingDecorator;
 import it.frontend.e2e.framework.core.model.selector.XPathSelector;
 import it.frontend.e2e.framework.web.adapter.IWebPresentationApiAdapter;
-import it.frontend.e2e.framework.web.adapter.model.FindPolicy;
 import it.frontend.e2e.framework.web.model.WebPresentationElement;
 import it.frontend.e2e.framework.web.model.location.Url;
 
-import java.util.List;
 import java.util.Optional;
 
 public class WebAdapterLoggingDecorator extends AbstractAdapterLoggingDecorator<XPathSelector, Url, WebPresentationElement> implements IWebPresentationApiAdapter {
@@ -21,49 +19,37 @@ public class WebAdapterLoggingDecorator extends AbstractAdapterLoggingDecorator<
 
     @Override
     public Optional<String> getCookieValue(String name) {
-        logger.logDebug("Get cookie value by name: " + name);
+        logger.logInfo("Get cookie value by name: " + name);
         return webAdapter.getCookieValue(name);
     }
 
     @Override
-    public Optional<WebPresentationElement> findElement(XPathSelector selector, FindPolicy findPolicy) {
-        logger.logDebug("Find element by selector: " + selector + " with find policy: " + findPolicy);
-        return webAdapter.findElement(selector, findPolicy);
-    }
-
-    @Override
-    public Optional<List<WebPresentationElement>> findElements(XPathSelector selector, FindPolicy policy) {
-        logger.logDebug("Find elements by selector: " + selector + " with find policy: " + policy);
-        return webAdapter.findElements(selector, policy);
-    }
-
-    @Override
     public Optional<String> getLocalStorageItem(String key) {
-        logger.logDebug("Get localStorage item by key: " + key);
+        logger.logInfo("Get localStorage item by key: " + key);
         return webAdapter.getLocalStorageItem(key);
     }
 
     @Override
     public Optional<String> getSessionStorageItem(String key) {
-        logger.logDebug("Get sessionStorage item by key: " + key);
+        logger.logInfo("Get sessionStorage item by key: " + key);
         return webAdapter.getSessionStorageItem(key);
     }
 
     @Override
     public void setLocalStorageItem(String key, String value) {
-        logger.logDebug("Set localStorage item: " + key + " = " + value);
+        logger.logInfo("Set localStorage item: " + key + " = " + value);
         webAdapter.setLocalStorageItem(key, value);
     }
 
     @Override
     public void setSessionStorageItem(String key, String value) {
-        logger.logDebug("Set sessionStorage item: " + key + " = " + value);
+        logger.logInfo("Set sessionStorage item: " + key + " = " + value);
         webAdapter.setSessionStorageItem(key, value);
     }
 
     @Override
     public void close() {
-        logger.logDebug("Closing Browser");
+        logger.logInfo("Closing Browser");
         webAdapter.close();
     }
 }
