e364c42912403ed483b7b8954e9a2690dea97149
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-31 21:42:34 +0200

    feat: [QA-16442] enhance AgreementSteps to include tenant context for deprecated EService handling

--- interop/src/main/java/it/pagopa/interop/common/agreement/infrastructure/cucumber/AgreementSteps.java
+++ interop/src/main/java/it/pagopa/interop/common/agreement/infrastructure/cucumber/AgreementSteps.java
@@ -6,11 +6,15 @@ import it.pagopa.interop.common.agreement.application.AgreementUseCase;
 import it.pagopa.interop.common.agreement.domain.AgreementCreationFailureReason;
 import it.pagopa.interop.common.eservice.domain.EService;
 import it.pagopa.interop.common.eservice.domain.EServiceDescriptor;
+import it.pagopa.interop.common.kernel.context.CurrentUserSession;
+import it.pagopa.interop.common.kernel.domain.Tenant;
+import it.pagopa.interop.common.kernel.domain.User;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class AgreementSteps {
     private final AgreementUseCase agreementUseCase;
+    private final CurrentUserSession currentUserSession;
 
     @Given("associa un Agreement in stato DRAFT all'{currentEService}")
     public void createAgreement(EService eService) {
@@ -18,8 +22,8 @@ public class AgreementSteps {
     }
 
     @When("il sistema impedisce a/al {tenant} di inoltrare una richiesta di fruizione per la {currentDeprecatedEServiceDescriptor} dell'{currentEService}")
-    public void createAgreement(EServiceDescriptor eServiceDescriptor, EService eService) {
+    public void createAgreement(Tenant consumer, EServiceDescriptor eServiceDescriptor, EService eService) {
+        currentUserSession.set(User.getTenantAdmin(consumer), consumer);
         agreementUseCase.shouldFailToCreateAgreement(eService, eServiceDescriptor, AgreementCreationFailureReason.DEPRECATED_VERSION);
     }
-
 }
