1624d31b551a8ece1715efd208f3c7a902c64bed
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-08-31 21:40:14 +0200

    feat: [QA-16442] update navigateTo method to accept path parameters and resolve URLs dynamically

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/impl/LocatableCapabilityImpl.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/capability/impl/LocatableCapabilityImpl.java
@@ -4,14 +4,51 @@ import it.frontend.e2e.framework.core.capability.core.Locatable;
 import it.frontend.e2e.framework.web.adapter.IWebPresentationApiAdapter;
 import it.frontend.e2e.framework.web.model.location.Url;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LocatableCapabilityImpl extends AbstractCapabilityImpl implements Locatable {
     public LocatableCapabilityImpl(IWebPresentationApiAdapter adapter) {
         super(adapter);
     }
 
     @Override
-    public void navigateTo() {
+    public void navigateTo(String... pathParams) {
         Url url = urlSupplier.get();
-        adapter.navigateTo(url);
+        String urlTemplate = url.getUrl();
+
+        Url result = resolveUrl(pathParams, urlTemplate);
+        adapter.navigateTo(result);
+    }
+
+    protected Url resolveUrl(String[] pathParams, String urlTemplate) {
+        Matcher matcher = Pattern.compile("\\$\\{[^}]+}").matcher(urlTemplate);
+
+        int placeholdersCount = 0;
+        while (matcher.find()) {
+            placeholdersCount++;
+        }
+
+        if (placeholdersCount != pathParams.length) {
+            throw new IllegalArgumentException(
+                    "Path parameters mismatch: expected " + placeholdersCount
+                            + ", provided " + pathParams.length
+            );
+        }
+
+        matcher = Pattern.compile("\\$\\{[^}]+}").matcher(urlTemplate);
+
+        StringBuilder resolvedUrl = new StringBuilder();
+        int index = 0;
+
+        while (matcher.find()) {
+            matcher.appendReplacement(
+                    resolvedUrl,
+                    Matcher.quoteReplacement(pathParams[index++])
+            );
+        }
+
+        matcher.appendTail(resolvedUrl);
+        return Url.of(resolvedUrl.toString());
     }
 }
