775520f5f52ccc6b70d7e29cd72beb993691ecc4
veldoralegiuseppe <veldorale.giuseppe@gmail.com>
2026-09-01 21:21:59 +0200

    refactor: [QA-16998] remove unused DataTableResolutionStrategy implementations

--- interop/src/main/java/it/pagopa/interop/common/infrastructure/cucumber/resolver/DataTableResolutionStrategy.java
+++
@@ -1,6 +0,0 @@
-package it.pagopa.interop.common.infrastructure.cucumber.resolver;
-
-public interface DataTableResolutionStrategy {
-    String getFunctionName();
-    String resolve(String argument);
-}
