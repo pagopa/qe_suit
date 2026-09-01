e223e668b2d89f9b7a8ed55cdbf32ef425c06073
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-01 09:22:23 +0200

    feat : [QA-16522] other refactor test for preconditions of PIN 10444

--- interop/src/main/java/it/pagopa/interop/common/eservice/infrastructure/cucumber/EServiceSteps.java
+++ interop/src/main/java/it/pagopa/interop/common/eservice/infrastructure/cucumber/EServiceSteps.java
@@ -1,32 +1,48 @@
 package it.pagopa.interop.common.eservice.infrastructure.cucumber;
 
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import it.pagopa.interop.common.eservice.application.EServiceDescriptorGateway;
 import it.pagopa.interop.common.eservice.application.EServiceUseCase;
 import it.pagopa.interop.common.eservice.domain.EService;
-import it.pagopa.interop.common.infrastructure.context.cucumber.UserContext;
+import it.pagopa.interop.common.eservice.domain.EServiceDescriptor;
 import it.pagopa.interop.common.kernel.context.CurrentUserSession;
+import it.pagopa.interop.common.kernel.context.EntityStore;
 import it.pagopa.interop.common.kernel.domain.Tenant;
 import it.pagopa.interop.common.kernel.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 public class EServiceSteps {
     private final EServiceUseCase eServiceUseCase;
+    private final EServiceDescriptorGateway eServiceDescriptorGateway;
     private final CurrentUserSession userContext;
+    private final EntityStore entityStore;
 
     @When("{tenant} crea una nuova versione del {currentEService}")
     public void createNewEserviceVersion(Tenant tenant, EService eService){
         userContext.set(User.getTenantAdmin(tenant), tenant);
         eServiceUseCase.addDescriptor(eService);
+
+        EServiceDescriptor descriptor = eService.getDescriptors().get(eService.getDescriptors().size()-1);
+        String openApiInterface = "assets/origin-interface.yaml";
+        eServiceDescriptorGateway.linkOpenApiInterface(eService.getRef(), descriptor.getRef(), openApiInterface);
+        eServiceDescriptorGateway.publishDescriptor(eService.getRef(), descriptor.getRef());
     }
 
     @Given("il {tenant} consulta la pagina dell'eservice")
     public void ilPotenzialeFruitoreConsultaLaPaginaDellEservice(Tenant potenzialeFruitore) {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("il fruitore " + potenzialeFruitore + " fa cose ...");
+        EService eService = entityStore.getLastOrThrow(EService.class);
+
+        UUID eserviceId = eService.getId();
+        UUID lastDescriptorId = eService.getDescriptors()
+                .get(eService.getDescriptors().size() - 1)
+                .getId();
+        // ... naviga alla pagina web usando eserviceId e lastDescriptorId
     }
 
     @Then("il {tenant} trova il pulsante di richiesta di fruizione disabilitato per tutte le versioni antecedenti l'ultima")
