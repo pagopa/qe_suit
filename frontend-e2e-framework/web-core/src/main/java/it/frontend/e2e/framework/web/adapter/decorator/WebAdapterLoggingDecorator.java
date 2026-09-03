99a9bed59c0c39303590ec35e9a35c4abcf3efa0
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-09-03 15:10:27 +0200

    refactor: [QA-17047] introduce FindPolicy enum and update findElement method to support different element search strategies

--- frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/decorator/WebAdapterLoggingDecorator.java
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/decorator/WebAdapterLoggingDecorator.java
@@ -3,6 +3,7 @@ package it.frontend.e2e.framework.web.adapter.decorator;
 import it.frontend.e2e.framework.core.adapter.decorator.AbstractAdapterLoggingDecorator;
 import it.frontend.e2e.framework.core.model.selector.XPathSelector;
 import it.frontend.e2e.framework.web.adapter.IWebPresentationApiAdapter;
+import it.frontend.e2e.framework.web.adapter.model.FindPolicy;
 import it.frontend.e2e.framework.web.model.WebPresentationElement;
 import it.frontend.e2e.framework.web.model.location.Url;
 
@@ -19,37 +20,43 @@ public class WebAdapterLoggingDecorator extends AbstractAdapterLoggingDecorator<
 
     @Override
     public Optional<String> getCookieValue(String name) {
-        logger.logInfo("Get cookie value by name: " + name);
+        logger.logDebug("Get cookie value by name: " + name);
         return webAdapter.getCookieValue(name);
     }
 
     @Override
+    public Optional<WebPresentationElement> findElement(XPathSelector selector, FindPolicy findPolicy) {
+        logger.logDebug("Find element by selector: " + selector + " with find policy: " + findPolicy);
+        return webAdapter.findElement(selector, findPolicy);
+    }
+
+    @Override
     public Optional<String> getLocalStorageItem(String key) {
-        logger.logInfo("Get localStorage item by key: " + key);
+        logger.logDebug("Get localStorage item by key: " + key);
         return webAdapter.getLocalStorageItem(key);
     }
 
     @Override
     public Optional<String> getSessionStorageItem(String key) {
-        logger.logInfo("Get sessionStorage item by key: " + key);
+        logger.logDebug("Get sessionStorage item by key: " + key);
         return webAdapter.getSessionStorageItem(key);
     }
 
     @Override
     public void setLocalStorageItem(String key, String value) {
-        logger.logInfo("Set localStorage item: " + key + " = " + value);
+        logger.logDebug("Set localStorage item: " + key + " = " + value);
         webAdapter.setLocalStorageItem(key, value);
     }
 
     @Override
     public void setSessionStorageItem(String key, String value) {
-        logger.logInfo("Set sessionStorage item: " + key + " = " + value);
+        logger.logDebug("Set sessionStorage item: " + key + " = " + value);
         webAdapter.setSessionStorageItem(key, value);
     }
 
     @Override
     public void close() {
-        logger.logInfo("Closing Browser");
+        logger.logDebug("Closing Browser");
         webAdapter.close();
     }
 }
---
+++ frontend-e2e-framework/web-core/src/main/java/it/frontend/e2e/framework/web/adapter/model/FindPolicy.java
@@ -0,0 +1,7 @@
+package it.frontend.e2e.framework.web.adapter.model;
+
+public enum FindPolicy {
+    CLICKABLE,
+    VISIBLE,
+    PRESENT
+}
