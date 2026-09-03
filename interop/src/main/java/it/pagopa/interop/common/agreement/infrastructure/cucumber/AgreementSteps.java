b83765cb9519e66f4224ba05007a0de0de40e020
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-03 16:00:03 +0200

    fix : [QA-15533] added ARCHIVED_STATE and fixed the behavior on the @Then step where different reasons may be considered

--- interop/src/main/java/it/pagopa/interop/common/agreement/infrastructure/cucumber/AgreementSteps.java
+++ interop/src/main/java/it/pagopa/interop/common/agreement/infrastructure/cucumber/AgreementSteps.java
@@ -21,9 +21,16 @@ public class AgreementSteps {
         agreementUseCase.createAgreement(eService, eService.getLastDraftDescriptor());
     }
 
+    @When("il sistema impedisce a/al {tenant} di inoltrare una richiesta di fruizione per la {currentArchivedEServiceDescriptor} dell'{currentEService}")
     @When("il sistema impedisce a/al {tenant} di inoltrare una richiesta di fruizione per la {currentDeprecatedEServiceDescriptor} dell'{currentEService}")
     public void createAgreement(Tenant consumer, EServiceDescriptor eServiceDescriptor, EService eService) {
         currentUserSession.set(User.getTenantAdmin(consumer), consumer);
-        agreementUseCase.shouldFailToCreateAgreement(eService, eServiceDescriptor, AgreementCreationFailureReason.DEPRECATED_VERSION);
+        AgreementCreationFailureReason reason = switch (eServiceDescriptor.getState()){
+            case DRAFT,SUSPENDED,ARCHIVING_SUSPENDED, WAITING_FOR_APPROVAL -> AgreementCreationFailureReason.ESERVICE_INVALID_STATE;
+            case DEPRECATED -> AgreementCreationFailureReason.DEPRECATED_VERSION;
+            case ARCHIVED -> AgreementCreationFailureReason.ARCHIVED_STATE;
+            default -> throw new IllegalArgumentException("Status not among the ones to be handled");
+        };
+        agreementUseCase.shouldFailToCreateAgreement(eService, eServiceDescriptor, reason);
     }
 }
