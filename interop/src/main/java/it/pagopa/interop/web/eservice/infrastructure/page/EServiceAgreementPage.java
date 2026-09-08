8de67ae973cf1892df351c03a814c3c1ca97cc95
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-08 15:54:28 +0200

    feat : [QA-10665] remove comments and tidy up a bit

--- interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/page/EServiceAgreementPage.java
+++ interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/page/EServiceAgreementPage.java
@@ -39,20 +39,14 @@ public interface EServiceAgreementPage extends Page {
                     .as("Breadcrumbs last item text")
                     .isEqualTo("Gestisci richiesta");
 
-            softly.assertThat(
-                    banner1().isPresent()
-            );
-
+            softly.assertThat(banner1())
+                    .as("Banner1 should be present")
+                    .isPresent();
 
             final String message = "Questa versione dell’e-service è obsoleta, ma è ancora attiva. È disponibile una nuova versione.";
-            softly.assertThat(
-                    banner1().get().readAndAssert(text ->
-                            Assertions.assertThat(text)
-                                    .as("Banner1 text")
-                                    .contains(message)
-                                    .hasSize(message.length())
-                    )
-            );
+            softly.assertThat(banner1().map(Readable::read).orElse(null))
+                    .as("Banner1 text")
+                    .isEqualTo(message);
         });
     }
 
