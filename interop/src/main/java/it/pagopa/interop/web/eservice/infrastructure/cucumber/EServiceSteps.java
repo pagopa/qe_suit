3f1dd1a7eb13c80d02945c46ecdc36610a72ff30
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-11 11:45:32 +0200

    feat : [QA-17180][PIN-10046] add EServiceDetailErogatorePage and update EServiceSteps for handling empty attribute saving

--- interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/cucumber/EServiceSteps.java
+++ interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/cucumber/EServiceSteps.java
@@ -4,25 +4,26 @@ import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import it.pagopa.interop.common.eservice.domain.EService;
+import it.pagopa.interop.web.eservice.infrastructure.page.EServiceDetailErogatorePage;
 import it.pagopa.interop.web.eservice.infrastructure.page.EServiceDetailPage;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class EServiceSteps {
 
-    final private EServiceDetailPage eServiceDetailPage;
+    final private EServiceDetailErogatorePage eServiceDetailErogatorePage;
 
     @When("il Comune di Milano tenta di salvare attributi vuoti per l'{currentEService}")
     public void ilComuneDiMilanoTentaDiSalvareAttributiVuotiPerLEService(EService eService) {
         String eserviceId = String.valueOf(eService.getId());
         String descriptorId = String.valueOf(eService.getDescriptors().get(0).getId());
-        eServiceDetailPage.navigateTo(eserviceId, descriptorId);
-        eServiceDetailPage.assertLoaded();
+        eServiceDetailErogatorePage.navigateTo(eserviceId, descriptorId);
+        eServiceDetailErogatorePage.assertLoaded();
     }
 
     @Then("il sistema non ritorna un messaggio di errore")
     public void ilSistemaNonRitornaUnMessaggioDiErrore() {
-        throw new PendingException();
+        eServiceDetailErogatorePage.assertNoErrorNotification();
     }
 
 }
