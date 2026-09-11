9035b73537f6de2630ec834f3d18a8b4097834c5
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-11 09:37:54 +0200

    feat : [QA-17180][PIN-10046] add EServiceSteps for handling empty attribute saving scenario

---
+++ interop/src/main/java/it/pagopa/interop/web/eservice/infrastructure/cucumber/EServiceSteps.java
@@ -0,0 +1,30 @@
+package it.pagopa.interop.web.eservice.infrastructure.cucumber;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import it.pagopa.interop.common.eservice.domain.EService;
+import it.pagopa.interop.web.eservice.infrastructure.page.EServiceDetailPage;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class EServiceSteps {
+
+    EServiceDetailPage eServiceDetailPage;
+
+    @When("il Comune di Milano tenta di salvare attributi vuoti per l'{currentEService}")
+    public void ilComuneDiMilanoTentaDiSalvareAttributiVuotiPerLEService(EService eService) {
+        String eserviceId = String.valueOf(eService.getId());
+        String descriptorId = String.valueOf(eService.getDescriptors().get(0).getId());
+        eServiceDetailPage.navigateTo(eserviceId, descriptorId);
+        eServiceDetailPage.assertLoaded();
+    }
+
+    @Then("il sistema non ritorna un messaggio di errore")
+    public void ilSistemaNonRitornaUnMessaggioDiErrore() {
+        throw new PendingException();
+    }
+
+}
