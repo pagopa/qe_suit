2fa319987aca76d1d4fd2e21904e9e8240130c50
Vincenzo-Massaro <vincenzo.massaro@alten.it>
2026-09-21 17:33:01 +0200

    refactor(url-injection): [PIN-6881] [QA-17190] generazione valori contract test attributi

--- common/src/main/java/it/pagopa/utils/RandomUtils.java
+++ common/src/main/java/it/pagopa/utils/RandomUtils.java
@@ -1,19 +1,24 @@
 package it.pagopa.utils;
 
-import org.instancio.Instancio;
-
-import static org.instancio.Select.allStrings;
+import org.apache.commons.lang3.RandomStringUtils;
 
 public class RandomUtils {
     private RandomUtils() {
     }
 
-    public static String randomAlphanumericName(String prefix,  int length) {
-        return Instancio.of(String.class)
-                .generate(allStrings(), gen -> gen.string().prefix(prefix+"-").length(length)).create();
+    public static String randomAlphanumeric(int length) {
+        return RandomStringUtils.insecure().nextAlphanumeric(length);
+    }
+
+    public static String randomAlphanumeric() {
+        return randomAlphanumeric(15);
+    }
+
+    public static String randomAlphanumericName(String prefix, int length) {
+        return prefix + "-" + randomAlphanumeric(length);
     }
 
     public static String randomAlphanumericName(String prefix) {
-       return randomAlphanumericName(prefix, 15);
+        return randomAlphanumericName(prefix, 15);
     }
 }
