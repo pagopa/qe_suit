bff93f01a2f4e6c82258e78c63c73600f4f53a8e
giuseppe.veldorale <giuseppe.veldorale@pagopa.it>
2026-09-24 00:31:13 +0200

    feat(BffClientContractTest): [QA-17489] replace ClientPathParams record with a map for client ID

--- interop/src/test/java/it/pagopa/interop/suite/contract/BffClientContractTest.java
+++ interop/src/test/java/it/pagopa/interop/suite/contract/BffClientContractTest.java
@@ -14,7 +14,6 @@ import it.pagopa.interop.common.kernel.domain.UserRef;
 import it.pagopa.interop.common.kernel.domain.UserRole;
 import it.pagopa.interop.generated.openapi.clients.bff.ApiClient;
 import it.pagopa.utils.RandomUtils;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
@@ -31,8 +30,6 @@ import java.util.stream.Stream;
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @RequiredArgsConstructor
 public class BffClientContractTest {
-    public record ClientPathParams(UUID clientId) {}
-
     private final ApiClient apiClient;
     private final HttpContractValidator httpContractValidator;
     private final InteropJourney interopJourney;
@@ -69,7 +66,7 @@ public class BffClientContractTest {
                             )
                             .get(Client.class);
 
-                    return new ClientPathParams(createdClient.getId());
+                    return Map.of("clientId", createdClient.getId());
                 })
                 .tests();
     }
