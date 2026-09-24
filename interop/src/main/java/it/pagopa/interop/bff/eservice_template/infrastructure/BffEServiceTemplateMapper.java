73c9b09adddac244865bedb9c6ea113c7f4005f0
Luigi Gjoni <luigi.gjoni@alten.it>
2026-09-24 11:27:47 +0200

    fix : [QA-17180] [PIN-10046] fix of COPILOT REVIEW for mapper

--- interop/src/main/java/it/pagopa/interop/bff/eservice_template/infrastructure/BffEServiceTemplateMapper.java
+++ interop/src/main/java/it/pagopa/interop/bff/eservice_template/infrastructure/BffEServiceTemplateMapper.java
@@ -20,7 +20,8 @@ public interface BffEServiceTemplateMapper {
     @Mapping(target = "updatedAt", ignore = true)
     @Mapping(target = "version", ignore = true)
     @Mapping(target = "versions", ignore = true)
-        @Mapping(target = "riskAnalyses", ignore = true)
+    @Mapping(target = "riskAnalyses", ignore = true)
+    @Mapping(target = "riskAnalysis", ignore = true)
     EServiceTemplate toEServiceTemplateBase(EServiceTemplateDetails source);
 }
 
